715b1360c8686f01bd3e2656be6ac83a2420a1e4
Arthur Kenichi Condino Odaguiri <46831317+Arthur-Kenichi-Condino@users.noreply.github.com>
2022-05-02 03:11:02 -0300

    ...

--- Assets/AKCondinoO/Sims/PersistentDataLoadingMultithreaded.cs
+++ Assets/AKCondinoO/Sims/PersistentDataLoadingMultithreaded.cs
@@ -1,13 +1,17 @@
 #if UNITY_EDITOR
     #define ENABLE_LOG_DEBUG
 #endif
+using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.IO;
 using UnityEngine;
 namespace AKCondinoO.Sims{
     internal class PersistentDataLoadingBackgroundContainer:BackgroundContainer{
     }
-    internal class PersistentDataLoadingMultithreaded:BaseMultithreaded<PersistentDataLoadingBackgroundContainer>{ 
+    internal class PersistentDataLoadingMultithreaded:BaseMultithreaded<PersistentDataLoadingBackgroundContainer>{
+     internal readonly Dictionary<Type,FileStream>fileStream=new Dictionary<Type,FileStream>();
+      internal readonly Dictionary<Type,StreamReader>fileStreamReader=new Dictionary<Type,StreamReader>();
         protected override void Execute(){
         }
     }
