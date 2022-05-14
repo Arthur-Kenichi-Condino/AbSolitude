ff8ce905460405111154bd56a1197b837b204554
Arthur Kenichi Condino Odaguiri <46831317+Arthur-Kenichi-Condino@users.noreply.github.com>
2022-05-14 02:34:02 -0300

    ...
    Former-commit-id: d36e802102535b2ad92589ba7a1ae65b71aaede1

---
+++ Assets/AKCondinoO/UI/Fixed/BuildBuyEditMode/BuildCategory/Tables/TableFloors.cs
@@ -0,0 +1,7 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+namespace AKCondinoO.UI.Fixed.BuildBuyEditMode.BuildCategory.Tables{
+    internal class TableFloors:BuildCategorySimObjectsTable{
+    }
+}
