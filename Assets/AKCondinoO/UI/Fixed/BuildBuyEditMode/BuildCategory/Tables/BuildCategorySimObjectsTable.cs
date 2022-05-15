c6ca22a3c0d9fa538cea57be1cce6e0d3369f18b
Arthur Kenichi Condino Odaguiri <46831317+Arthur-Kenichi-Condino@users.noreply.github.com>
2022-05-15 01:17:43 -0300

    Update BuildCategorySimObjectsTable.cs
    Former-commit-id: be81f8f5ae5371c4b328a81b7b656a93da98c9b2

--- Assets/AKCondinoO/UI/Fixed/BuildBuyEditMode/BuildCategory/Tables/BuildCategorySimObjectsTable.cs
+++ Assets/AKCondinoO/UI/Fixed/BuildBuyEditMode/BuildCategory/Tables/BuildCategorySimObjectsTable.cs
@@ -1,3 +1,6 @@
+#if UNITY_EDITOR
+    #define ENABLE_LOG_DEBUG
+#endif
 using AKCondinoO.Sims;
 using System.Collections;
 using System.Collections.Generic;
@@ -6,6 +9,11 @@ namespace AKCondinoO.UI.Fixed.BuildBuyEditMode.BuildCategory.Tables{
     internal class BuildCategorySimObjectsTable:MonoBehaviour{
      internal readonly SortedDictionary<string,SimObject>tableSimObjects=new SortedDictionary<string,SimObject>();
      internal void OnCreateTable(){
+      foreach(var typeNameSimObjectPair in tableSimObjects){
+       string typeName=typeNameSimObjectPair.Key;
+       SimObject simObject=typeNameSimObjectPair.Value;
+       Log.DebugMessage("OnCreateTable:"+typeName+"..."+simObject);
+      }
      }
     }
 }
