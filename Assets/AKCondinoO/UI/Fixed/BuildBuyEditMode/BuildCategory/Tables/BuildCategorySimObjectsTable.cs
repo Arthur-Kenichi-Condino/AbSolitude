4651c3a794070ec2e192f77cc022d78cdeb8bdd5
Arthur Kenichi Condino Odaguiri <46831317+Arthur-Kenichi-Condino@users.noreply.github.com>
2022-05-16 23:23:26 -0300

    ...

--- Assets/AKCondinoO/UI/Fixed/BuildBuyEditMode/BuildCategory/Tables/BuildCategorySimObjectsTable.cs
+++ Assets/AKCondinoO/UI/Fixed/BuildBuyEditMode/BuildCategory/Tables/BuildCategorySimObjectsTable.cs
@@ -19,16 +19,33 @@ namespace AKCondinoO.UI.Fixed.BuildBuyEditMode.BuildCategory.Tables{
            TraverseHierarchy(child);
           }else if(child.name=="Viewport"){
            TraverseHierarchy(child);
+          }else if(child.name=="Content"){
+           tableScrollViewContent=(RectTransform)child;
+           Log.DebugMessage("tableScrollViewContent set");
           }
          }
         }
         internal void OnCreateTable(){
+         RectTransform simObjectButtonPrefabRectTransform=(RectTransform)FixedUI.singleton.buildBuyEditModeUIContent.buildCategorySimObjectButtonPrefab.transform;
          int x=0;
          int y=0;
          foreach(var typeNameSimObjectPair in tableSimObjects){
           string typeName=typeNameSimObjectPair.Key;
           SimObject simObject=typeNameSimObjectPair.Value;
           Log.DebugMessage("OnCreateTable:"+typeName+"..."+simObject);
+          Vector3 pos=new Vector3(
+           simObjectButtonPrefabRectTransform.position.x+x*(simObjectButtonPrefabRectTransform.sizeDelta.x+10),
+           simObjectButtonPrefabRectTransform.position.y-y*(simObjectButtonPrefabRectTransform.sizeDelta.y+10),
+           0
+          );
+          GameObject simObjectButton=Instantiate(FixedUI.singleton.buildBuyEditModeUIContent.buildCategorySimObjectButtonPrefab,tableScrollViewContent.transform);
+          Log.DebugMessage("simObjectButton pos:"+pos);
+          simObjectButton.transform.localPosition=pos;
+          x++;
+          if(x>1){
+           x=0;
+           y++;
+          }
          }
         }
     }
