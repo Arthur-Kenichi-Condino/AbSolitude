c36ca7a4a4e59d0bf7fffebef5b632259a6f9023
Arthur Kenichi Condino Odaguiri <46831317+Arthur-Kenichi-Condino@users.noreply.github.com>
2022-05-30 05:08:16 -0300

    ...

--- Assets/AKCondinoO/Util.cs
+++ Assets/AKCondinoO/Util.cs
@@ -6,5 +6,16 @@ namespace AKCondinoO{
     internal class Util{
         internal static void SetUtil(){
         }
+        internal static void DrawWireCapsule(Vector3 p1,Vector3 p2,float radius){
+         #if UNITY_EDITOR
+             //  Special case when both points are in the same position
+             if(p1==p2){
+              //  DrawWireSphere works only in gizmos methods
+              Gizmos.DrawWireSphere(p1,radius);
+              return;
+             }
+             Gizmos.DrawLine(p1+Vector3.left*radius,p2+Vector3.left*radius);
+         #endif
+        }
     }
 }
--- Assets/Resources/AKCondinoO/Sims/Actors/ArthurCondino/ArthurCondinoAI.cs
+++ Assets/Resources/AKCondinoO/Sims/Actors/ArthurCondino/ArthurCondinoAI.cs
@@ -3,5 +3,22 @@ using System.Collections.Generic;
 using UnityEngine;
 namespace AKCondinoO.Sims.Actors.ArthurCondino{
     internal class ArthurCondinoAI:SimActor{
+        #if UNITY_EDITOR
+            protected override void OnDrawGizmos(){
+             DrawColliders();
+            }
+            void DrawColliders(){
+             if(interactionsEnabled){
+              foreach(Collider collider in volumeColliders){
+               if(collider is CapsuleCollider capsule){
+                Gizmos.color=Color.gray;
+                Gizmos.matrix=Matrix4x4.TRS(transform.position+capsule.center,transform.rotation,transform.localScale);
+                Util.DrawWireCapsule(Vector3.up*(capsule.height/2f-capsule.radius),Vector3.down*(capsule.height/2f-capsule.radius),capsule.radius);
+               }
+              }
+              Gizmos.matrix=Matrix4x4.identity;
+             }
+            }
+        #endif
     }
 }
