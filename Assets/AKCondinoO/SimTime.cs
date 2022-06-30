d2d8e0de0b33b57d8b26c510b9b20e12be40d705
Arthur Kenichi Condino Odaguiri <46831317+Arthur-Kenichi-Condino@users.noreply.github.com>
2022-06-30 01:33:56 -0300

    ...
    Former-commit-id: fd923b6e407231d4b1d244a9e4a2f9ac98f6a036

--- Assets/AKCondinoO/SimTime.cs
+++ Assets/AKCondinoO/SimTime.cs
@@ -6,5 +6,13 @@ using System.Collections.Generic;
 using UnityEngine;
 namespace AKCondinoO{
     internal class SimTime:MonoBehaviour{   
+     internal const int   _YEAR  =12;
+     internal const int   _MONTH =28;
+     internal       float _DAY      {get;private set;}
+     internal       float _HOUR     {get;private set;}
+     internal       float _MINUTE   {get;private set;}
+     internal const float _SECOND=1f;
+        void Awake(){
+        }
     }
 }
