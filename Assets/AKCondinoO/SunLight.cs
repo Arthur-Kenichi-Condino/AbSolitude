83acb16a2cb2f56f15cc78fa002efe7b6a75148e
Arthur Kenichi Condino Odaguiri <46831317+Arthur-Kenichi-Condino@users.noreply.github.com>
2022-07-01 00:56:01 -0300

    ...

--- Assets/AKCondinoO/SunLight.cs
+++ Assets/AKCondinoO/SunLight.cs
@@ -3,7 +3,36 @@ using System.Collections.Generic;
 using UnityEngine;
 namespace AKCondinoO{
     internal class SunLight:MonoBehaviour{
+     float sunLightIntensityClearNight =0f ;
+     float sunLightIntensityClearSunset=.5f;
+     float sunLightIntensityClearDawn  =.5f;
+     float sunLightIntensityClearDay   =1f ;
         internal void UpdateValues(float transitionLerp){
+         if(SimTime.singleton.simDayTransition==SimTime.DayTransitions.Night){
+          OnNightTime(transitionLerp);
+         }else if(SimTime.singleton.simDayTransition==SimTime.DayTransitions.DayToSunset){
+          OnDayToSunsetTransition(transitionLerp);
+         }else if(SimTime.singleton.simDayTransition==SimTime.DayTransitions.SunsetToNight){
+          OnSunsetToNightTransition(transitionLerp);
+         }else if(SimTime.singleton.simDayTransition==SimTime.DayTransitions.NightToDawn){
+          OnNightToDawnTransition(transitionLerp);
+         }else if(SimTime.singleton.simDayTransition==SimTime.DayTransitions.DawnToDay){
+          OnDawnToDayTransition(transitionLerp);
+         }else{
+          OnDayTime(transitionLerp);
+         }
+        }
+        void OnNightTime(float transitionLerp){
+        }
+        void OnDayToSunsetTransition(float transitionLerp){
+        }
+        void OnSunsetToNightTransition(float transitionLerp){
+        }
+        void OnNightToDawnTransition(float transitionLerp){
+        }
+        void OnDawnToDayTransition(float transitionLerp){
+        }
+        void OnDayTime(float transitionLerp){
         }
     }
 }
