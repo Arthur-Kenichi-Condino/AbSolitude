1ed246d5162b0a726e9332290f13a68ad635803d
Arthur Kenichi Condino Odaguiri <46831317+Arthur-Kenichi-Condino@users.noreply.github.com>
2022-07-01 01:21:44 -0300

    ...
    Former-commit-id: ad2e83208e4caa05fa5d01b7275c578eafbef55a

--- Assets/AKCondinoO/SunTransform.cs
+++ Assets/AKCondinoO/SunTransform.cs
@@ -6,10 +6,10 @@ using System.Collections.Generic;
 using UnityEngine;
 namespace AKCondinoO{
     internal class SunTransform:MonoBehaviour{
-     internal Vector3 mainSunEulerAngles=new Vector3(50.0f,-90.0f,0.0f);
+     [SerializeField]internal Vector3 sunEulerAngles=new Vector3(50.0f,-90.0f,0.0f);
         internal void UpdateValues(){
-         mainSunEulerAngles.x=SimTime.singleton.simDayCourse*360f-90f;
-         transform.rotation=Quaternion.Euler(mainSunEulerAngles);
+         sunEulerAngles.x=SimTime.singleton.simDayCourse*360f-90f;
+         transform.rotation=Quaternion.Euler(sunEulerAngles);
         }
     }
 }
