fda3f32e5e19835834b065f8234907896aa641ff
Arthur Kenichi Condino Odaguiri <46831317+Arthur-Kenichi-Condino@users.noreply.github.com>
2022-07-20 02:12:02 -0300

    Update BaseBiomeSimObjectsSpawnSettings.cs
    Former-commit-id: 189228ac493cc6e01ef0a1b3e9e30c775cc16690

--- Assets/AKCondinoO/Voxels/Biomes/BaseBiomeSimObjectsSpawnSettings.cs
+++ Assets/AKCondinoO/Voxels/Biomes/BaseBiomeSimObjectsSpawnSettings.cs
@@ -14,35 +14,40 @@ namespace AKCondinoO.Voxels.Biomes{
          internal Vector3 minSpacing;
          internal Vector3 maxSpacing;
         }
-     readonly protected Dictionary<Type,SimObjectSettings[]>allSettings=new Dictionary<Type,SimObjectSettings[]>();
+     readonly protected Dictionary<int,HashSet<Type>>simObjectPicking=new Dictionary<int,HashSet<Type>>();
+     readonly protected Dictionary<Type,Dictionary<int,List<SimObjectSettings>>>allSettings=new Dictionary<Type,Dictionary<int,List<SimObjectSettings>>>();
      readonly BaseBiome biome;
         internal BaseBiomeSimObjectsSpawnSettings(BaseBiome biome){
          this.biome=biome;
-         allSettings.Add(
-          typeof(Pinus_elliottii_1),
-          new SimObjectSettings[]{
-           new SimObjectSettings{
-            chance=.125f,
-            inclination=.125f,
-            minScale=Vector3.one*.5f,
-            maxScale=Vector3.one*1.5f,
-            depth=1.2f,
-            minSpacing=Vector3.one*2.4f,
-            maxSpacing=Vector3.one*4.8f,
-           },
-          }
-         );
          if(!simObjectPicking.TryGetValue(1,out var types1)){
-          types1=simObjectPicking[1]=new List<Type>();
+          types1=simObjectPicking[1]=new HashSet<Type>();
          }
          types1.Add(typeof(Pinus_elliottii_1));
+         if(!allSettings.TryGetValue(typeof(Pinus_elliottii_1),out var Pinus_elliottii_1Settings)){
+          Pinus_elliottii_1Settings=allSettings[typeof(Pinus_elliottii_1)]=new Dictionary<int,List<SimObjectSettings>>();
+         }
+         if(!Pinus_elliottii_1Settings.TryGetValue(1,out var Pinus_elliottii_1SettingsList1)){
+          Pinus_elliottii_1SettingsList1=Pinus_elliottii_1Settings[1]=new List<SimObjectSettings>();
+         }
+         Pinus_elliottii_1SettingsList1.Add(
+          new SimObjectSettings{
+           chance=.125f,
+           inclination=.125f,
+           minScale=Vector3.one*.5f,
+           maxScale=Vector3.one*1.5f,
+           depth=1.2f,
+           minSpacing=Vector3.one*2.4f,
+           maxSpacing=Vector3.one*4.8f,
+          }
+         );
         }
-     readonly protected Dictionary<int,List<Type>>simObjectPicking=new Dictionary<int,List<Type>>();
         internal(Type simObject,SimObjectSettings simObjectSettings)?TrySpawnSimObject(Vector3Int noiseInputRounded){
          Vector3 noiseInput=noiseInputRounded+biome.deround;
          int selection=biome.Selection(noiseInput);
          if(simObjectPicking.TryGetValue(selection,out var types)){
-          //SimObjectSettings simObjectSettings=allSettings[];
+          foreach(var type in types){
+           //SimObjectSettings simObjectSettings=allSettings[type][];
+          }
          }
          return null;
         }
