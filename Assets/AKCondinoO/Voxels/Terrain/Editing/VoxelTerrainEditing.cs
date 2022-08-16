7c70b2bda3ab3192f2f42b07b5f684f82b0e1a58
Arthur Kenichi Condino Odaguiri <46831317+Arthur-Kenichi-Condino@users.noreply.github.com>
2022-08-16 17:49:05 -0300

    ...
    Former-commit-id: a1e6c42defc6ad569a48b3c4406bc251b5b399af

---
+++ Assets/AKCondinoO/Voxels/Terrain/Editing/VoxelTerrainEditing.cs
@@ -0,0 +1,11 @@
+#if UNITY_EDITOR
+    #define ENABLE_DEBUG_GIZMOS
+    #define ENABLE_LOG_DEBUG
+#endif
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+namespace AKCondinoO.Voxels.Terrain.Editing{
+    internal class VoxelTerrainEditing:MonoBehaviour{
+    }
+}
