0c4792c1eba84f803e889c8953691c1b6640af74
Arthur Kenichi Condino Odaguiri <46831317+Arthur-Kenichi-Condino@users.noreply.github.com>
2022-08-30 00:20:11 -0300

    ...
    Former-commit-id: 88407e824a40ce1a5c11ae32f459c8263de41cfc

--- Assets/AKCondinoO/Sims/Actors/BaseAI.cs
+++ Assets/AKCondinoO/Sims/Actors/BaseAI.cs
@@ -1,55 +1,19 @@
 #if UNITY_EDITOR
-    #define ENABLE_LOG_DEBUG
+#define ENABLE_LOG_DEBUG
 #endif
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.AI;
 using static AKCondinoO.Voxels.VoxelSystem;
-namespace AKCondinoO.Sims.Actors{
-    internal class BaseAI:SimActor{
-        internal enum ActorMotion:int{
-         MOTION_STAND=0,
-         MOTION_MOVE =1,
-        }
+namespace AKCondinoO.Sims.Actors {
+ internal partial class BaseAI:SimActor{
      protected ActorMotion MyMotion=ActorMotion.MOTION_STAND;
       internal ActorMotion motion{get{return MyMotion;}}
-        internal enum State:int{
-         IDLE_ST  =0,
-         FOLLOW_ST=1,
-        }
      protected State MyState=State.IDLE_ST;
       internal State state{get{return MyState;}}
-        internal enum PathfindingResult:int{
-         IDLE                   =0,
-         REACHED                =1,
-         PENDING                =2,
-         TRAVELLING             =3,
-         TRAVELLING_BUT_NO_SPEED=4,
-        }
-        PathfindingResult GetPathfindingResult(){
-         if(navMeshAgent.pathPending){
-          return PathfindingResult.PENDING;
-         }
-         if(!navMeshAgent.hasPath){
-          return PathfindingResult.IDLE;
-         }
-         if(
-          navMeshAgent.remainingDistance==Mathf.Infinity||
-          navMeshAgent.remainingDistance==float.NaN     ||
-          navMeshAgent.remainingDistance<0
-         ){
-          return PathfindingResult.IDLE;
-         }
-         if(navMeshAgent.remainingDistance>navMeshAgent.stoppingDistance){
-          if(Mathf.Approximately(navMeshAgent.velocity.sqrMagnitude,0f)){
-           return PathfindingResult.TRAVELLING_BUT_NO_SPEED;
-          }
-          return PathfindingResult.TRAVELLING;
-         }
-         return PathfindingResult.REACHED;
-        }
      protected PathfindingResult MyPathfinding=PathfindingResult.IDLE;
+      internal PathfindingResult pathfinding{get{return MyPathfinding;}}
         protected override void AI(){
          base.AI();
          MyPathfinding=GetPathfindingResult();
@@ -57,6 +21,10 @@ namespace AKCondinoO.Sims.Actors{
          if(MyState==State.FOLLOW_ST){
          }else{
           OnIDLE_ST();
+         }
+         if(navMeshAgent.velocity!=Vector3.zero){
+          float velocityMagnitude=navMeshAgent.velocity.magnitude;
+          Log.DebugMessage("navMeshAgent velocityMagnitude:"+velocityMagnitude);
          }
         }
      [SerializeField]protected float delayToRandomMove=8.0f;
@@ -73,6 +41,11 @@ namespace AKCondinoO.Sims.Actors{
            Log.DebugMessage("can do random movement");
            if(GetRandomPosition(transform.position,8.0f,out Vector3 result)){
             Log.DebugMessage("got random position:"+result);
+            if(navMeshAgentShouldUseRunSpeed){
+             navMeshAgent.speed=navMeshAgentRunSpeed;
+            }else{
+             navMeshAgent.speed=navMeshAgentWalkSpeed;
+            }
             navMeshAgent.destination=result;
            }
           }
---
+++ Assets/AKCondinoO/Sims/Actors/BaseAI/BaseAI.ActorMotion.cs
@@ -0,0 +1,11 @@
+﻿#if UNITY_EDITOR
+    #define ENABLE_LOG_DEBUG
+#endif
+namespace AKCondinoO.Sims.Actors{
+    internal partial class BaseAI{
+        internal enum ActorMotion:int{
+         MOTION_STAND=0,
+         MOTION_MOVE =1,
+        }
+    }
+}
