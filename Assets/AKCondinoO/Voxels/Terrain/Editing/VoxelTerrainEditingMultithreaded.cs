3e0a18f39708733ecda1149aaabcf22993c973e8
Arthur Kenichi Condino Odaguiri <46831317+Arthur-Kenichi-Condino@users.noreply.github.com>
2022-09-01 14:20:25 -0300

    ...

--- Assets/AKCondinoO/Voxels/Terrain/Editing/VoxelTerrainEditingMultithreaded.cs
+++ Assets/AKCondinoO/Voxels/Terrain/Editing/VoxelTerrainEditingMultithreaded.cs
@@ -3,15 +3,28 @@
 #endif
 using System.Collections;
 using System.Collections.Generic;
+using System.Threading;
 using UnityEngine;
 using static AKCondinoO.Voxels.Terrain.Editing.VoxelTerrainEditing;
 namespace AKCondinoO.Voxels.Terrain.Editing{
     internal class VoxelTerrainEditingContainer:BackgroundContainer{
+     internal object[]terrainSynchronization;
      internal readonly Queue<TerrainEditRequest>requests=new Queue<TerrainEditRequest>();
     }
     internal class VoxelTerrainEditingMultithreaded:BaseMultithreaded<VoxelTerrainEditingContainer>{
         protected override void Execute(){
          Log.DebugMessage("VoxelTerrainEditingMultithreaded:Execute()");
+         foreach(object syn in container.terrainSynchronization){
+          Monitor.Enter(syn);
+         }
+         try{
+         }catch{
+          throw;
+         }finally{
+          foreach(object syn in container.terrainSynchronization){
+           Monitor.Exit(syn);
+          }
+         }
         }
     }
 }
