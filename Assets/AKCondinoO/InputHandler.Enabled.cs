13c554de192e34a43ff9f397f50c5f78b20a958f
Arthur Kenichi Condino Odaguiri <46831317+Arthur-Kenichi-Condino@users.noreply.github.com>
2022-09-03 21:16:17 -0300

    ...
    Former-commit-id: e6aded22ef7ffd8527a1a0d04a44b047ef61d067

--- Assets/AKCondinoO/InputHandler.Enabled.cs
+++ Assets/AKCondinoO/InputHandler.Enabled.cs
@@ -6,6 +6,11 @@ namespace AKCondinoO{
         internal static class Enabled{
          internal static readonly float[]MOUSE_ROTATION_DELTA_X={0,0};
          internal static readonly float[]MOUSE_ROTATION_DELTA_Y={0,0};
+         internal static readonly object[]PAUSE={true,true};
+         internal static readonly object[]FORWARD ={false,false};
+         internal static readonly object[]BACKWARD={false,false};
+         internal static readonly object[]RIGHT   ={false,false};
+         internal static readonly object[]LEFT    ={false,false};
         }
     }
 }
