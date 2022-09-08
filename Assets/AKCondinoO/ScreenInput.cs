e5bda9d8272ea5704717e4a51b9d22f5fdb241c7
Arthur Kenichi Condino Odaguiri <46831317+Arthur-Kenichi-Condino@users.noreply.github.com>
2022-09-07 21:03:20 -0300

    ...

--- Assets/AKCondinoO/ScreenInput.cs
+++ Assets/AKCondinoO/ScreenInput.cs
@@ -5,16 +5,32 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.EventSystems;
 namespace AKCondinoO{
     internal class ScreenInput:MonoBehaviour,ISingletonInitialization{
      internal static ScreenInput singleton{get;set;}
         void Awake(){
          if(singleton==null){singleton=this;}else{DestroyImmediate(this);return;}
         }
+     internal bool isPointerOverUIElement;
+     internal PointerEventData pointerEventData;
+      internal readonly List<RaycastResult>eventSystemRaycastResults=new List<RaycastResult>();
         public void Init(){
+         pointerEventData=new PointerEventData(EventSystem.current);
         }
         public void OnDestroyingCoreEvent(object sender,EventArgs e){
          Log.DebugMessage("ScreenInput:OnDestroyingCoreEvent");
+        }
+        void Update(){
+         pointerEventData.position=Input.mousePosition;
+         isPointerOverUIElement=false;
+         eventSystemRaycastResults.Clear();
+         EventSystem.current.RaycastAll(pointerEventData,eventSystemRaycastResults);
+         foreach(var raycastResult in eventSystemRaycastResults){
+          if(raycastResult.gameObject.layer==Util.UILayer){
+           isPointerOverUIElement=true;
+          }
+         }
         }
     }
 }
