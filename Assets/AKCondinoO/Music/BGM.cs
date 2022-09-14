34587a90187e36f98d2446cec8a5ca5f60fe19b6
Arthur Kenichi Condino Odaguiri <46831317+Arthur-Kenichi-Condino@users.noreply.github.com>
2022-09-14 20:55:37 -0300

    doing BGM system
    Former-commit-id: 8529094849335a19646b878ef15c5c641cb56b86

--- Assets/AKCondinoO/Music/BGM.cs
+++ Assets/AKCondinoO/Music/BGM.cs
@@ -8,13 +8,31 @@ using UnityEngine;
 namespace AKCondinoO.Music{
     internal class BGM:MonoBehaviour,ISingletonInitialization{
      internal static BGM singleton{get;set;}
+     internal AudioSource audioSource;
         private void Awake(){
          if(singleton==null){singleton=this;}else{DestroyImmediate(this);return;}
+         audioSource=GetComponent<AudioSource>();
         }
         public void Init(){
+         waitForNewMusic=new WaitUntil(()=>{return newMusic!=null;});
+         BGMusicCoroutine=StartCoroutine(PlayBGMusic());
         }
         public void OnDestroyingCoreEvent(object sender,EventArgs e){
          Log.DebugMessage("BGM:OnDestroyingCoreEvent");
+         if(this!=null){
+          StopCoroutine(BGMusicCoroutine);
+         }
+        }
+     Coroutine BGMusicCoroutine;
+     internal AudioClip newMusic=null;
+      WaitUntil waitForNewMusic;
+        private IEnumerator PlayBGMusic(){
+            Loop:{
+             yield return waitForNewMusic;
+             Log.DebugMessage("PlayBGMusic:newMusic:"+newMusic);
+             newMusic=null;
+            }
+            goto Loop;
         }
     }
 }
