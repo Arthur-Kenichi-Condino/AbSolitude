713079034ad8d2de098ba837d93866726a37d65d
Arthur Kenichi Condino Odaguiri <46831317+Arthur-Kenichi-Condino@users.noreply.github.com>
2022-09-15 05:05:12 -0300

    added bgm for day/night cycle
    Former-commit-id: d23123b0d12df2f7cd9665d546c7caf530f76725

--- Assets/AKCondinoO/Music/BGM.cs
+++ Assets/AKCondinoO/Music/BGM.cs
@@ -9,15 +9,16 @@ namespace AKCondinoO.Music{
     internal class BGM:MonoBehaviour,ISingletonInitialization{
      internal static BGM singleton{get;set;}
      internal AudioSource audioSource;
-      [SerializeField]AudioClip GoodMorningMusic;
-      [SerializeField]AudioClip RushingNoonMusic;
-      [SerializeField]AudioClip SpookyNightMusic;
+      [SerializeField]internal AudioClip GoodMorningMusic;
+      [SerializeField]internal AudioClip RushingNoonMusic;
+      [SerializeField]internal AudioClip SpookyNightMusic;
         private void Awake(){
          if(singleton==null){singleton=this;}else{DestroyImmediate(this);return;}
          audioSource=GetComponent<AudioSource>();
         }
         public void Init(){
          waitForNewMusic=new WaitUntil(()=>{return newMusic!=null;});
+         fadeTimeInterval=new WaitForSeconds(0.05f);
          BGMusicCoroutine=StartCoroutine(PlayBGMusic());
         }
         public void OnDestroyingCoreEvent(object sender,EventArgs e){
@@ -29,10 +30,20 @@ namespace AKCondinoO.Music{
      Coroutine BGMusicCoroutine;
      internal AudioClip newMusic=null;
       WaitUntil waitForNewMusic;
+       WaitForSeconds fadeTimeInterval;
         private IEnumerator PlayBGMusic(){
             Loop:{
              yield return waitForNewMusic;
              Log.DebugMessage("PlayBGMusic:newMusic:"+newMusic);
+             if(audioSource.isPlaying){
+              for(float vol;audioSource.volume>0f;vol=audioSource.volume-.1f,audioSource.volume=vol<=0f?0f:vol){
+               yield return fadeTimeInterval;
+              }
+              audioSource.Stop();
+             }
+             audioSource.clip=newMusic;
+             audioSource.volume=1f;
+             audioSource.Play();
              newMusic=null;
             }
             goto Loop;
