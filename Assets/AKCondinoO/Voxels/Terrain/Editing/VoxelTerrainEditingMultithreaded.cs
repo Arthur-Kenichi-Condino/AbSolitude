635a33f6e87454e12e7e6cb28ea58d0972ade1bd
Arthur Kenichi Condino Odaguiri <46831317+Arthur-Kenichi-Condino@users.noreply.github.com>
2022-10-16 05:26:27 -0300

    terrain edit: cube with smooth borders
    TO DO: validade values before start [or there may be math errors like dividing by 0]
    TO DO: PlaceSphere but validade values before start
    
    
    Former-commit-id: 5b9726b8694213072466b144a62ff1b4b38d2196

--- Assets/AKCondinoO/Voxels/Terrain/Editing/VoxelTerrainEditingMultithreaded.cs
+++ Assets/AKCondinoO/Voxels/Terrain/Editing/VoxelTerrainEditingMultithreaded.cs
@@ -75,30 +75,21 @@ namespace AKCondinoO.Voxels.Terrain.Editing{
           Vector3Int size      =editRequest.size;
           double     density   =editRequest.density;
           MaterialId material  =editRequest.material;
-          int        smoothness=editRequest.smoothness;
+          Vector3Int smoothness=editRequest.smoothness;
           switch(editRequest.mode){
            case(EditMode.PlaceCube):{
             Log.DebugMessage("EditMode.PlaceCube");
-                //  calcular valores para suavização
-                float sqrt_yx_1=Mathf.Sqrt(Mathf.Pow(size.y,2)+Mathf.Pow(size.x,2));
-                float sqrt_xz_1=Mathf.Sqrt(Mathf.Pow(size.x,2)+Mathf.Pow(size.z,2));
-                float sqrt_zy_1=Mathf.Sqrt(Mathf.Pow(size.z,2)+Mathf.Pow(size.y,2));
-                 float sqrt_yx_xz_1=Mathf.Sqrt(Mathf.Pow(sqrt_yx_1,2)+Mathf.Pow(sqrt_xz_1,2));
-                  float sqrt_yx_xz_zy_1=Mathf.Sqrt(Mathf.Pow(sqrt_yx_xz_1,2)+Mathf.Pow(sqrt_zy_1,2));
-                float sqrt_yx_2;
-                float sqrt_xz_2;
-                float sqrt_zy_2;
+                //  TO DO: calcular valores para suavização
                 Vector2Int cCoord1=vecPosTocCoord(center ),        cCoord3;
                 Vector2Int cnkRgn1=cCoordTocnkRgn(cCoord1),        cnkRgn3;
                 Vector3Int vCoord1=vecPosTovCoord(center ),vCoord2,vCoord3;
                 //  y
-                for(int y=0;y<size.y+smoothness;++y){for(vCoord2=new Vector3Int(vCoord1.x,vCoord1.y-y,vCoord1.z);vCoord2.y<=vCoord1.y+y;vCoord2.y+=y*2){
+                for(int y=0;y<size.y;++y){for(vCoord2=new Vector3Int(vCoord1.x,vCoord1.y-y,vCoord1.z);vCoord2.y<=vCoord1.y+y;vCoord2.y+=y*2){
                         if(vCoord2.y>=0&&vCoord2.y<Height){
                 //  x
-                for(int x=0;x<size.x+smoothness;++x){for(vCoord2.x=vCoord1.x-x                                  ;vCoord2.x<=vCoord1.x+x;vCoord2.x+=x*2){
-                         sqrt_yx_2=Mathf.Sqrt(Mathf.Pow(y,2)+Mathf.Pow(x,2));
+                for(int x=0;x<size.x;++x){for(vCoord2.x=vCoord1.x-x                                  ;vCoord2.x<=vCoord1.x+x;vCoord2.x+=x*2){
                 //  z
-                for(int z=0;z<size.z+smoothness;++z){for(vCoord2.z=vCoord1.z-z                                  ;vCoord2.z<=vCoord1.z+z;vCoord2.z+=z*2){
+                for(int z=0;z<size.z;++z){for(vCoord2.z=vCoord1.z-z                                  ;vCoord2.z<=vCoord1.z+z;vCoord2.z+=z*2){
                          cCoord3=cCoord1;
                          cnkRgn3=cnkRgn1;
                          vCoord3=vCoord2;
@@ -109,25 +100,39 @@ namespace AKCondinoO.Voxels.Terrain.Editing{
                           cCoord3=cnkRgnTocCoord(cnkRgn3);
                          }
                          int cnkIdx3=GetcnkIdx(cCoord3.x,cCoord3.y);
-                         sqrt_xz_2=Mathf.Sqrt(Mathf.Pow(x,2)+Mathf.Pow(z,2));
-                         sqrt_zy_2=Mathf.Sqrt(Mathf.Pow(z,2)+Mathf.Pow(y,2));
-                         double resultDensity;
-                         if(y>=size.y||x>=size.x||z>=size.z){
-                          if(y>=size.y&&x>=size.x&&z>=size.z){
-                           float sqrt_yx_xz_2=Mathf.Sqrt(Mathf.Pow(sqrt_yx_2,2)+Mathf.Pow(sqrt_xz_2,2));
-                            float sqrt_yx_xz_zy_2=Mathf.Sqrt(Mathf.Pow(sqrt_yx_xz_2,2)+Mathf.Pow(sqrt_zy_2,2));
-                           resultDensity=density*(1f-(sqrt_yx_xz_zy_2-sqrt_yx_xz_1)/(sqrt_yx_xz_zy_2));
-                          }else if(y>=size.y&&x>=size.x){resultDensity=density*(1f-(sqrt_yx_2-sqrt_yx_1)/(sqrt_yx_2));
-                          }else if(x>=size.x&&z>=size.z){resultDensity=density*(1f-(sqrt_xz_2-sqrt_xz_1)/(sqrt_xz_2));
-                          }else if(z>=size.z&&y>=size.y){resultDensity=density*(1f-(sqrt_zy_2-sqrt_zy_1)/(sqrt_zy_2));
-                          }else if(y>=size.y){resultDensity=density*(1f-(y-size.y)/(float)y)*1.414f;//  raiz quadrada de 2
-                          }else if(x>=size.x){resultDensity=density*(1f-(x-size.x)/(float)x)*1.414f;
-                          }else if(z>=size.z){resultDensity=density*(1f-(z-size.z)/(float)z)*1.414f;
-                          }else{
-                           resultDensity=0d;
+                         double resultDensity=density;
+                         if(smoothness.x>1&&
+                            smoothness.y>1&&
+                            smoothness.z>1)
+                         {
+                          float hardness=1f;
+                          Vector3Int smoothnessBeginDis=smoothness-Vector3Int.one;
+                             void SmoothDensity(float dis,float totalDis,float beginDis){
+                              float value=(totalDis-dis)/(beginDis+1f);
+                              value=(value+1f)/2f;
+                              value*=.99f;
+                              hardness*=value;
+                             }
+                          if(y>=smoothnessBeginDis.y){
+                           SmoothDensity(y,size.y,smoothnessBeginDis.y);
+                           if(x>=smoothnessBeginDis.x){
+                            SmoothDensity(x,size.x,smoothnessBeginDis.x);
+                            if(z>=smoothnessBeginDis.z){
+                             SmoothDensity(z,size.z,smoothnessBeginDis.z);
+                            }
+                           }else if(z>=smoothnessBeginDis.z){
+                            SmoothDensity(z,size.z,smoothnessBeginDis.z);
+                           }
+                          }else if(x>=smoothnessBeginDis.x){
+                           SmoothDensity(x,size.x,smoothnessBeginDis.x);
+                           if(z>=smoothnessBeginDis.z){
+                            SmoothDensity(z,size.z,smoothnessBeginDis.z);
+                           }
+                          }else if(z>=smoothnessBeginDis.z){
+                           SmoothDensity(z,size.z,smoothnessBeginDis.z);
                           }
-                         }else{
-                          resultDensity=density;
+                          Log.DebugMessage("hardness:"+hardness);
+                          resultDensity*=hardness;
                          }
                          //  TO DO: get current file data to merge
                          if(!dataFromFileToMerge.ContainsKey(cCoord3)){
@@ -220,6 +225,41 @@ namespace AKCondinoO.Voxels.Terrain.Editing{
                         }
                  if(y==0){break;}
                 }}
+            break;
+           }
+           case(EditMode.PlaceSphere):{
+            Log.DebugMessage("EditMode.PlaceSphere");
+                //float sqrt_yx_1=Mathf.Sqrt(Mathf.Pow(size.y,2)+Mathf.Pow(size.x,2));
+                //float sqrt_xz_1=Mathf.Sqrt(Mathf.Pow(size.x,2)+Mathf.Pow(size.z,2));
+                //float sqrt_zy_1=Mathf.Sqrt(Mathf.Pow(size.z,2)+Mathf.Pow(size.y,2));
+                // float sqrt_yx_xz_1=Mathf.Sqrt(Mathf.Pow(sqrt_yx_1,2)+Mathf.Pow(sqrt_xz_1,2));
+                //  float sqrt_yx_xz_zy_1=Mathf.Sqrt(Mathf.Pow(sqrt_yx_xz_1,2)+Mathf.Pow(sqrt_zy_1,2));
+                //float sqrt_yx_2;
+                //float sqrt_xz_2;
+                //float sqrt_zy_2;
+                
+                         //sqrt_yx_2=Mathf.Sqrt(Mathf.Pow(y,2)+Mathf.Pow(x,2));
+                         
+                         //sqrt_xz_2=Mathf.Sqrt(Mathf.Pow(x,2)+Mathf.Pow(z,2));
+                         //sqrt_zy_2=Mathf.Sqrt(Mathf.Pow(z,2)+Mathf.Pow(y,2));
+                         
+                         //if(y>=size.y||x>=size.x||z>=size.z){
+                         // if(y>=size.y&&x>=size.x&&z>=size.z){
+                         //  float sqrt_yx_xz_2=Mathf.Sqrt(Mathf.Pow(sqrt_yx_2,2)+Mathf.Pow(sqrt_xz_2,2));
+                         //   float sqrt_yx_xz_zy_2=Mathf.Sqrt(Mathf.Pow(sqrt_yx_xz_2,2)+Mathf.Pow(sqrt_zy_2,2));
+                         //  resultDensity=density*(1f-(sqrt_yx_xz_zy_2-sqrt_yx_xz_1)/(sqrt_yx_xz_zy_2));
+                         // }else if(y>=size.y&&x>=size.x){resultDensity=density*(1f-(sqrt_yx_2-sqrt_yx_1)/(sqrt_yx_2));
+                         // }else if(x>=size.x&&z>=size.z){resultDensity=density*(1f-(sqrt_xz_2-sqrt_xz_1)/(sqrt_xz_2));
+                         // }else if(z>=size.z&&y>=size.y){resultDensity=density*(1f-(sqrt_zy_2-sqrt_zy_1)/(sqrt_zy_2));
+                         // }else if(y>=size.y){resultDensity=density*(1f-(y-size.y)/(float)y)*1.414f;//  raiz quadrada de 2
+                         // }else if(x>=size.x){resultDensity=density*(1f-(x-size.x)/(float)x)*1.414f;
+                         // }else if(z>=size.z){resultDensity=density*(1f-(z-size.z)/(float)z)*1.414f;
+                         // }else{
+                         //  resultDensity=0d;
+                         // }
+                         //}else{
+                         // resultDensity=density;
+                         //}
             break;
            }
           }
