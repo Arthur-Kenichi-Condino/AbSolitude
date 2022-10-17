991cfaf237b2b46b9893ef15712ea8efa953fc53
Arthur Kenichi Condino Odaguiri <46831317+Arthur-Kenichi-Condino@users.noreply.github.com>
2022-10-17 02:05:40 -0300

    terrain place sphere

--- Assets/AKCondinoO/Voxels/Terrain/Editing/VoxelTerrainEditingMultithreaded.cs
+++ Assets/AKCondinoO/Voxels/Terrain/Editing/VoxelTerrainEditingMultithreaded.cs
@@ -80,7 +80,6 @@ namespace AKCondinoO.Voxels.Terrain.Editing{
            case(EditMode.PlaceCube):{
             Log.DebugMessage("EditMode.PlaceCube");
             //  TO DO: validade values before start
-                //  TO DO: calcular valores para suavização
                 Vector2Int cCoord1=vecPosTocCoord(center ),        cCoord3;
                 Vector2Int cnkRgn1=cCoordTocnkRgn(cCoord1),        cnkRgn3;
                 Vector3Int vCoord1=vecPosTovCoord(center ),vCoord2,vCoord3;
@@ -136,89 +135,7 @@ namespace AKCondinoO.Voxels.Terrain.Editing{
                           resultDensity*=hardness;
                          }
                          //  TO DO: get current file data to merge
-                         if(!dataFromFileToMerge.ContainsKey(cCoord3)){
-                          if(!terrainEditOutputDataPool.TryDequeue(out Dictionary<Vector3Int,TerrainEditOutputData>editData)){
-                           editData=new Dictionary<Vector3Int,TerrainEditOutputData>();
-                          }
-                          dataFromFileToMerge.Add(cCoord3,editData);
-                          //  TO DO: load data here
-                          string fileName=string.Format(CultureInfoUtil.en_US,VoxelTerrainEditing.terrainEditingFileFormat,VoxelTerrainEditing.terrainEditingPath,cCoord3.x,cCoord3.y);
-                          if(File.Exists(fileName)){
-                           FileStream fileStream=new FileStream(fileName,FileMode.Open,FileAccess.Read,FileShare.ReadWrite);
-                           StreamReader fileStreamReader=new StreamReader(fileStream);
-                           fileStream.Position=0L;
-                           fileStreamReader.DiscardBufferedData();
-                           string line;
-                           while((line=fileStreamReader.ReadLine())!=null){
-                            if(string.IsNullOrEmpty(line)){continue;}
-                            int vCoordStringStart=line.IndexOf("vCoord=(");
-                            if(vCoordStringStart>=0){
-                               vCoordStringStart+=8;
-                             int vCoordStringEnd=line.IndexOf(") , ",vCoordStringStart);
-                             string vCoordString=line.Substring(vCoordStringStart,vCoordStringEnd-vCoordStringStart);
-                             //Log.DebugMessage("vCoordString:"+vCoordString);
-                             string[]xyzString=vCoordString.Split(',');
-                             int vCoordx=int.Parse(xyzString[0].Replace(" ",""),NumberStyles.Any,CultureInfoUtil.en_US);
-                             int vCoordy=int.Parse(xyzString[1].Replace(" ",""),NumberStyles.Any,CultureInfoUtil.en_US);
-                             int vCoordz=int.Parse(xyzString[2].Replace(" ",""),NumberStyles.Any,CultureInfoUtil.en_US);
-                             Vector3Int vCoord=new Vector3Int(vCoordx,vCoordy,vCoordz);
-                             if(!editData.ContainsKey(vCoord)){
-                              int editStringStart=vCoordStringEnd+4;
-                              editStringStart=line.IndexOf("terrainEditOutputData=",editStringStart);
-                              if(editStringStart>=0){
-                               int editStringEnd=line.IndexOf(" , }",editStringStart)+4;
-                               string editString=line.Substring(editStringStart,editStringEnd-editStringStart);
-                               //Log.DebugMessage("editString:"+editString);
-                               TerrainEditOutputData edit=TerrainEditOutputData.Parse(editString);
-                               editData.Add(vCoord,edit);
-                              }
-                             }
-                            }
-                           }
-                           fileStream      .Dispose();
-                           fileStreamReader.Dispose();
-                          }
-                         }
-                         Voxel currentVoxel;
-                         if(dataFromFileToMerge.ContainsKey(cCoord3)&&dataFromFileToMerge[cCoord3].ContainsKey(vCoord3)){
-                          TerrainEditOutputData voxelData=dataFromFileToMerge[cCoord3][vCoord3];
-                          currentVoxel=new Voxel(voxelData.density,Vector3.zero,voxelData.material);
-                         }else{
-                          currentVoxel=new Voxel();
-                          Vector3Int noiseInput=vCoord3;noiseInput.x+=cnkRgn3.x;
-                                                        noiseInput.z+=cnkRgn3.y;
-                          VoxelSystem.biome.Setvxl(
-                           noiseInput,
-                            null,
-                             null,
-                              0,
-                               vCoord3.z+vCoord3.x*Depth,
-                                ref currentVoxel
-                          );
-                         }
-                         resultDensity=Math.Max(resultDensity,currentVoxel.density);
-                         if(material==MaterialId.Air&&!(-resultDensity>=-isoLevel)){
-                          resultDensity=-resultDensity;
-                         }
-                         if(!dataForSavingToFile.ContainsKey(cCoord3)){
-                          if(!terrainEditOutputDataPool.TryDequeue(out Dictionary<Vector3Int,TerrainEditOutputData>editData)){
-                           editData=new Dictionary<Vector3Int,TerrainEditOutputData>();
-                          }
-                          dataForSavingToFile.Add(cCoord3,editData);
-                         }
-                         dataForSavingToFile[cCoord3][vCoord3]=new TerrainEditOutputData(resultDensity,-resultDensity>=-isoLevel?MaterialId.Air:material);
-                         //  TO DO: add neighbours that are dirty too
-                         for(int ngbx=-1;ngbx<=1;ngbx++){
-                         for(int ngby=-1;ngby<=1;ngby++){
-                          Vector2Int cCoord4=cCoord3+new Vector2Int(ngbx,ngby);
-                          if(Math.Abs(cCoord4.x)>=MaxcCoordx||
-                             Math.Abs(cCoord4.y)>=MaxcCoordy)
-                          {
-                           continue;
-                          }
-                          int cnkIdx4=GetcnkIdx(cCoord4.x,cCoord4.y);
-                          container.dirty.Add(cnkIdx4);
-                         }}
+                         MergeEdits(cCoord3,vCoord3,cnkRgn3,resultDensity,material);
                  if(z==0){break;}
                 }}
                  if(x==0){break;}
@@ -231,6 +148,52 @@ namespace AKCondinoO.Voxels.Terrain.Editing{
            case(EditMode.PlaceSphere):{
             Log.DebugMessage("EditMode.PlaceSphere");
             //  TO DO: PlaceSphere but validade values before start
+                Vector2Int cCoord1=vecPosTocCoord(center ),        cCoord3;
+                Vector2Int cnkRgn1=cCoordTocnkRgn(cCoord1),        cnkRgn3;
+                Vector3Int vCoord1=vecPosTovCoord(center ),vCoord2,vCoord3;
+                //  TO DO: calcular valores para suavização
+                //  calculate hypotenuses
+                float hyp_size_yx=Mathf.Sqrt(Mathf.Pow(size.y,2)+Mathf.Pow(size.x,2));
+                float hyp_size_xz=Mathf.Sqrt(Mathf.Pow(size.x,2)+Mathf.Pow(size.z,2));
+                float hyp_size_yx_xz=Mathf.Sqrt(Mathf.Pow(hyp_size_yx,2)+Mathf.Pow(hyp_size_xz,2));
+                //  y
+                for(int y=0;y<size.y;++y){for(vCoord2=new Vector3Int(vCoord1.x,vCoord1.y-y,vCoord1.z);vCoord2.y<=vCoord1.y+y;vCoord2.y+=y*2){
+                        if(vCoord2.y>=0&&vCoord2.y<Height){
+                //  x
+                for(int x=0;x<size.x;++x){for(vCoord2.x=vCoord1.x-x                                  ;vCoord2.x<=vCoord1.x+x;vCoord2.x+=x*2){
+                         float sqrt_yx_2=Mathf.Sqrt(Mathf.Pow(y,2)+Mathf.Pow(x,2));
+                //  z
+                for(int z=0;z<size.z;++z){for(vCoord2.z=vCoord1.z-z                                  ;vCoord2.z<=vCoord1.z+z;vCoord2.z+=z*2){
+                         cCoord3=cCoord1;
+                         cnkRgn3=cnkRgn1;
+                         vCoord3=vCoord2;
+                         if(vCoord3.x<0||vCoord3.x>=Width||
+                            vCoord3.z<0||vCoord3.z>=Depth
+                         ){
+                          ValidateCoord(ref cnkRgn3,ref vCoord3);
+                          cCoord3=cnkRgnTocCoord(cnkRgn3);
+                         }
+                         int cnkIdx3=GetcnkIdx(cCoord3.x,cCoord3.y);
+                         //  do sphere placing here
+                         double resultDensity=density;
+                         //  calculate hypotenuses
+                         float hyp_dis_xz=Mathf.Sqrt(Mathf.Pow(x,2)+Mathf.Pow(z,2));
+                         float hyp_dis_zy=Mathf.Sqrt(Mathf.Pow(z,2)+Mathf.Pow(y,2));
+                         float hyp_dis_yx_xz=Mathf.Sqrt(Mathf.Pow(sqrt_yx_2,2)+Mathf.Pow(hyp_dis_xz,2));
+                         float hyp_dis_yx_xz_zy=Mathf.Sqrt(Mathf.Pow(hyp_dis_yx_xz,2)+Mathf.Pow(hyp_dis_zy,2));
+                         if(hyp_size_yx_xz>0f){
+                          resultDensity*=(hyp_size_yx_xz-hyp_dis_yx_xz_zy)/hyp_size_yx_xz;
+                          Log.DebugMessage("(hyp_size_yx_xz-hyp_dis_yx_xz_zy)/hyp_size_yx_xz:"+((hyp_size_yx_xz-hyp_dis_yx_xz_zy)/hyp_size_yx_xz));
+                          Log.DebugMessage("resultDensity:"+resultDensity);
+                         }
+                         MergeEdits(cCoord3,vCoord3,cnkRgn3,resultDensity,material);
+                 if(z==0){break;}
+                }}
+                 if(x==0){break;}
+                }}
+                        }
+                 if(y==0){break;}
+                }}
                 //float sqrt_yx_1=Mathf.Sqrt(Mathf.Pow(size.y,2)+Mathf.Pow(size.x,2));
                 //float sqrt_xz_1=Mathf.Sqrt(Mathf.Pow(size.x,2)+Mathf.Pow(size.z,2));
                 //float sqrt_zy_1=Mathf.Sqrt(Mathf.Pow(size.z,2)+Mathf.Pow(size.y,2));
@@ -264,6 +227,94 @@ namespace AKCondinoO.Voxels.Terrain.Editing{
                          //}
             break;
            }
+          }
+         }
+         void MergeEdits(Vector2Int cCoord,Vector3Int vCoord,Vector2Int cnkRgn,double resultDensity,MaterialId material){
+          if(!dataFromFileToMerge.ContainsKey(cCoord)){
+           if(!terrainEditOutputDataPool.TryDequeue(out Dictionary<Vector3Int,TerrainEditOutputData>editData)){
+            editData=new Dictionary<Vector3Int,TerrainEditOutputData>();
+           }
+           dataFromFileToMerge.Add(cCoord,editData);
+           //  TO DO: load data here
+           LoadDataFromFile(cCoord,editData);
+          }
+          Voxel currentVoxel;
+          if(dataFromFileToMerge.ContainsKey(cCoord)&&dataFromFileToMerge[cCoord].ContainsKey(vCoord)){
+           TerrainEditOutputData voxelData=dataFromFileToMerge[cCoord][vCoord];
+           currentVoxel=new Voxel(voxelData.density,Vector3.zero,voxelData.material);
+          }else{
+           currentVoxel=new Voxel();
+           Vector3Int noiseInput=vCoord;noiseInput.x+=cnkRgn.x;
+                                        noiseInput.z+=cnkRgn.y;
+           VoxelSystem.biome.Setvxl(
+            noiseInput,
+             null,
+              null,
+               0,
+                vCoord.z+vCoord.x*Depth,
+                 ref currentVoxel
+           );
+          }
+          resultDensity=Math.Max(resultDensity,currentVoxel.density);
+          if(material==MaterialId.Air&&!(-resultDensity>=-isoLevel)){
+           resultDensity=-resultDensity;
+          }
+          if(!dataForSavingToFile.ContainsKey(cCoord)){
+           if(!terrainEditOutputDataPool.TryDequeue(out Dictionary<Vector3Int,TerrainEditOutputData>editData)){
+            editData=new Dictionary<Vector3Int,TerrainEditOutputData>();
+           }
+           dataForSavingToFile.Add(cCoord,editData);
+          }
+          dataForSavingToFile[cCoord][vCoord]=new TerrainEditOutputData(resultDensity,-resultDensity>=-isoLevel?MaterialId.Air:material);
+          //  TO DO: add neighbours that are dirty too
+          for(int ngbx=-1;ngbx<=1;ngbx++){
+          for(int ngby=-1;ngby<=1;ngby++){
+           Vector2Int cCoord4=cCoord+new Vector2Int(ngbx,ngby);
+           if(Math.Abs(cCoord4.x)>=MaxcCoordx||
+              Math.Abs(cCoord4.y)>=MaxcCoordy)
+           {
+            continue;
+           }
+           int cnkIdx4=GetcnkIdx(cCoord4.x,cCoord4.y);
+           container.dirty.Add(cnkIdx4);
+          }}
+         }
+         void LoadDataFromFile(Vector2Int cCoord,Dictionary<Vector3Int,TerrainEditOutputData>editData){
+          string fileName=string.Format(CultureInfoUtil.en_US,VoxelTerrainEditing.terrainEditingFileFormat,VoxelTerrainEditing.terrainEditingPath,cCoord.x,cCoord.y);
+          if(File.Exists(fileName)){
+           FileStream fileStream=new FileStream(fileName,FileMode.Open,FileAccess.Read,FileShare.ReadWrite);
+           StreamReader fileStreamReader=new StreamReader(fileStream);
+           fileStream.Position=0L;
+           fileStreamReader.DiscardBufferedData();
+           string line;
+           while((line=fileStreamReader.ReadLine())!=null){
+            if(string.IsNullOrEmpty(line)){continue;}
+            int vCoordStringStart=line.IndexOf("vCoord=(");
+            if(vCoordStringStart>=0){
+               vCoordStringStart+=8;
+             int vCoordStringEnd=line.IndexOf(") , ",vCoordStringStart);
+             string vCoordString=line.Substring(vCoordStringStart,vCoordStringEnd-vCoordStringStart);
+             //Log.DebugMessage("vCoordString:"+vCoordString);
+             string[]xyzString=vCoordString.Split(',');
+             int vCoordx=int.Parse(xyzString[0].Replace(" ",""),NumberStyles.Any,CultureInfoUtil.en_US);
+             int vCoordy=int.Parse(xyzString[1].Replace(" ",""),NumberStyles.Any,CultureInfoUtil.en_US);
+             int vCoordz=int.Parse(xyzString[2].Replace(" ",""),NumberStyles.Any,CultureInfoUtil.en_US);
+             Vector3Int vCoord=new Vector3Int(vCoordx,vCoordy,vCoordz);
+             if(!editData.ContainsKey(vCoord)){
+              int editStringStart=vCoordStringEnd+4;
+              editStringStart=line.IndexOf("terrainEditOutputData=",editStringStart);
+              if(editStringStart>=0){
+               int editStringEnd=line.IndexOf(" , }",editStringStart)+4;
+               string editString=line.Substring(editStringStart,editStringEnd-editStringStart);
+               //Log.DebugMessage("editString:"+editString);
+               TerrainEditOutputData edit=TerrainEditOutputData.Parse(editString);
+               editData.Add(vCoord,edit);
+              }
+             }
+            }
+           }
+           fileStream      .Dispose();
+           fileStreamReader.Dispose();
           }
          }
          foreach(object syn in container.terrainSynchronization){
