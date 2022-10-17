09d5b0873bac4bdff7ea6513aa28eb5e20d4c840
Arthur Kenichi Condino Odaguiri <46831317+Arthur-Kenichi-Condino@users.noreply.github.com>
2022-10-17 19:00:25 -0300

    ...

---
+++ Assets/AKCondinoO/Voxels/Water/VoxelWaterChunk.cs
@@ -0,0 +1,9 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+namespace AKCondinoO.Voxels.Water{
+    internal class VoxelWaterChunk:MonoBehaviour{
+        internal void ManualUpdate(){
+        }
+    }
+}
