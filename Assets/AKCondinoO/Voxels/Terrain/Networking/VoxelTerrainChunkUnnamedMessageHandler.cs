c143131d3b3d1eb0cafc0ae7a54851d927ad1131
Arthur Kenichi Condino Odaguiri <46831317+Arthur-Kenichi-Condino@users.noreply.github.com>
2022-10-21 07:12:58 -0300

    Update VoxelTerrainChunkUnnamedMessageHandler.cs

--- Assets/AKCondinoO/Voxels/Terrain/Networking/VoxelTerrainChunkUnnamedMessageHandler.cs
+++ Assets/AKCondinoO/Voxels/Terrain/Networking/VoxelTerrainChunkUnnamedMessageHandler.cs
@@ -8,6 +8,9 @@ using Unity.Netcode;
 using UnityEngine;
 namespace AKCondinoO.Voxels.Terrain.Networking{
     internal class VoxelTerrainChunkUnnamedMessageHandler:NetworkBehaviour{
+        void Awake(){
+         waitUntilGetFileData=new WaitUntil(()=>{return segmentCount>=0;});
+        }
         public override void OnNetworkSpawn(){
          base.OnNetworkSpawn();
          NetworkManager.CustomMessagingManager.OnUnnamedMessage+=OnReceivedUnnamedMessage;
@@ -21,30 +24,32 @@ namespace AKCondinoO.Voxels.Terrain.Networking{
          reader.ReadValueSafe(out messageType);
          if(messageType==(int)UnnamedMessageTypes.VoxelTerrainChunkcnkIdx){
           if(Core.singleton.isClient){
-           OnReceivedVoxelTerrainChunkcnkIdx(clientId,reader);
+           OnClientSideReceivedVoxelTerrainChunkcnkIdx(clientId,reader);
           }
          }else if(messageType==(int)UnnamedMessageTypes.VoxelTerrainChunkEditDataFileSegment){
           if(Core.singleton.isClient){
-           OnReceivedVoxelTerrainChunkEditDataFileSegment(clientId,reader);
+           OnClientSideReceivedVoxelTerrainChunkEditDataFileSegment(clientId,reader);
           }
          }
         }
-        void OnReceivedVoxelTerrainChunkcnkIdx(ulong clientId,FastBufferReader reader){
-         Log.DebugMessage("OnReceivedVoxelTerrainChunkcnkIdx");
+        void OnClientSideReceivedVoxelTerrainChunkcnkIdx(ulong clientId,FastBufferReader reader){
+         Log.DebugMessage("OnClientSideReceivedVoxelTerrainChunkcnkIdx");
          //  if this message fails to be received, client may ask for it after there's a missing MessageHandler for its chunk;
          //  spawn and update message handlers in the server with VoxelSystemNetworking
         }
-        void OnReceivedVoxelTerrainChunkEditDataFileSegment(ulong clientId,FastBufferReader reader){
-         Log.DebugMessage("OnReceivedVoxelTerrainChunkEditDataFileSegment");
+        void OnClientSideReceivedVoxelTerrainChunkEditDataFileSegment(ulong clientId,FastBufferReader reader){
+         Log.DebugMessage("OnClientSideReceivedVoxelTerrainChunkEditDataFileSegment");
          //  Validate segment with the cnkIdx in the message "header" by comparing it to the
          // current cnkIdx set to this MessageHandler
          //  if this message fails to be received, client may ask for it after detecting missing segments in the "dictionary" of
          // segment-data
         }
-        int segmentCount=-1;
-        internal IEnumerator SendVoxelTerrainChunkEditDataFileCoroutine(){
+     int segmentCount=-1;
+     WaitUntil waitUntilGetFileData;
+        internal IEnumerator ServerSideSendVoxelTerrainChunkEditDataFileCoroutine(){
             Loop:{
-             yield return null;
+             yield return waitUntilGetFileData;
+             segmentCount=-1;//  restart loop but don't repeat for the same edit data file
             }
             goto Loop;
         }
