0ce02cbcde6e4332bbe971e3cec09849b5405d94
Arthur Kenichi Condino Odaguiri <46831317+Arthur-Kenichi-Condino@users.noreply.github.com>
2022-10-23 05:03:12 -0300

    doing more network
    Former-commit-id: ac021e9fdade86f6889e439a52631aa4f2879509

--- Assets/AKCondinoO/Voxels/Terrain/Networking/VoxelTerrainGetFileEditDataToNetSyncMultithreaded.cs
+++ Assets/AKCondinoO/Voxels/Terrain/Networking/VoxelTerrainGetFileEditDataToNetSyncMultithreaded.cs
@@ -6,11 +6,13 @@ using System.Collections;
 using System.Collections.Generic;
 using Unity.Netcode;
 using UnityEngine;
+using static AKCondinoO.Voxels.Terrain.Editing.VoxelTerrainEditingMultithreaded;
 using static AKCondinoO.Voxels.Terrain.MarchingCubes.MarchingCubesTerrain;
+using static AKCondinoO.Voxels.Terrain.Networking.VoxelTerrainChunkUnnamedMessageHandler;
 using static AKCondinoO.Voxels.VoxelSystem;
 namespace AKCondinoO.Voxels.Terrain.Networking{
     internal class VoxelTerrainGetFileEditDataToNetSyncContainer:BackgroundContainer{
-     internal FastBufferWriter dataToSendToClients;
+     internal Dictionary<Vector3Int,TerrainEditOutputData>dataToSendToClients;
      internal Vector2Int cCoord;
      internal Vector2Int cnkRgn;
      internal        int cnkIdx;
@@ -19,28 +21,24 @@ namespace AKCondinoO.Voxels.Terrain.Networking{
         protected override void Execute(){
          VoxelSystem.Concurrent.terrainFileDatarwl.EnterReadLock();
          try{
-          Log.DebugMessage("VoxelTerrainGetFileEditDataToNetSyncMultithreaded:Execute:");
+          Log.DebugMessage("VoxelTerrainGetFileEditDataToNetSyncMultithreaded:Execute:VoxelsPerSegment:"+VoxelsPerSegment);
+          if(!dataToSendDictionaryPool.TryDequeue(out container.dataToSendToClients)){
+           container.dataToSendToClients=new Dictionary<Vector3Int,TerrainEditOutputData>();
+          }
+
+          //testing, REMOVE:
+             Vector3Int vCoord1;
+             for(vCoord1=new Vector3Int();vCoord1.y<Height;vCoord1.y++){
+             for(vCoord1.x=0             ;vCoord1.x<Width ;vCoord1.x++){
+             for(vCoord1.z=0             ;vCoord1.z<Depth ;vCoord1.z++){
+              container.dataToSendToClients[vCoord1]=new TerrainEditOutputData(0.0d,MaterialId.Air);
+             }}}
+
          }catch{
           throw;
          }finally{
           VoxelSystem.Concurrent.terrainFileDatarwl.ExitReadLock();
          }
-   
-             container.dataToSendToClients.WriteValueSafe((int)UnnamedMessageTypes.VoxelTerrainChunkEditDataFileSegment);
-             container.dataToSendToClients.WriteValueSafe((int)0);
-             container.dataToSendToClients.WriteValueSafe((int)0);
-             container.dataToSendToClients.WriteValueSafe((int)0);
-             Vector3Int vCoord1;
-             for(vCoord1=new Vector3Int();vCoord1.y<Height;vCoord1.y++){
-             for(vCoord1.x=0             ;vCoord1.x<Width ;vCoord1.x++){
-             for(vCoord1.z=0             ;vCoord1.z<Depth ;vCoord1.z++){
-              container.dataToSendToClients.WriteValueSafe(vCoord1.x);
-              container.dataToSendToClients.WriteValueSafe(vCoord1.y);
-              container.dataToSendToClients.WriteValueSafe(vCoord1.z);
-              container.dataToSendToClients.WriteValueSafe((double)0.0d);
-              container.dataToSendToClients.WriteValueSafe((ushort)MaterialId.Air);
-             }}}
-
         }
     }
 }
