88b562c11003de3eb709e485f34de2dbc97bf653
Arthur Kenichi Condino Odaguiri <46831317+Arthur-Kenichi-Condino@users.noreply.github.com>
2022-10-24 05:39:35 -0300

    ...
    Former-commit-id: f3cb9e17c293a4dad1cec102f0f2a8506a0e0e6a

---
+++ Assets/AKCondinoO/Voxels/Terrain/Networking/VoxelTerrainChunkUnnamedMessageHandler.Client.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.Netcode;
+using UnityEngine;
+namespace AKCondinoO.Voxels.Terrain.Networking{
+    internal partial class VoxelTerrainChunkUnnamedMessageHandler{
+        void OnClientSideReceivedVoxelTerrainChunkcnkIdx(ulong clientId,FastBufferReader reader){
+         Log.DebugMessage("OnClientSideReceivedVoxelTerrainChunkcnkIdx");
+         //  if this message fails to be received, client may ask for it after there's a missing MessageHandler for its chunk;
+         //  spawn and update message handlers in the server with VoxelSystemNetworking
+        }
+        void OnClientSideReceivedVoxelTerrainChunkEditDataFileSegment(ulong clientId,FastBufferReader reader){
+         //Log.DebugMessage("OnClientSideReceivedVoxelTerrainChunkEditDataFileSegment");
+          //testing, REMOVE:
+         //FastBufferReader dataToReceivedFromServer=new FastBufferReader(reader,Allocator.Persistent,-1,0,Allocator.Persistent);
+         //dataToReceivedFromServer.ReadValueSafe(out int readFirstValue);
+         ////Log.DebugMessage("readFirstValue:"+readFirstValue);
+         //Debug.Log("readFirstValue:"+readFirstValue);
+         //dataToReceivedFromServer.Dispose();
+         //  Validate segment with the cnkIdx in the message "header" by comparing it to the
+         // current cnkIdx set to this MessageHandler
+         //  if this message fails to be received, client may ask for it after detecting missing segments in the "dictionary" of
+         // segment-data
+        }
+    }
+}
