6ec1a1dd9575b89d977c6d7b0f329575b21d34d4
Arthur Kenichi Condino Odaguiri <46831317+Arthur-Kenichi-Condino@users.noreply.github.com>
2022-11-06 05:17:52 -0300

    ...
    Former-commit-id: 0f68dcebc3f1a5e6417223ab963055a35768d4cc

--- Assets/AKCondinoO/Voxels/Water/WaterSpreadingMultithreaded.cs
+++ Assets/AKCondinoO/Voxels/Water/WaterSpreadingMultithreaded.cs
@@ -1,29 +1,47 @@
 #if UNITY_EDITOR
     #define ENABLE_LOG_DEBUG
 #endif
+using System;
 using System.Collections;
 using System.Collections.Concurrent;
 using System.Collections.Generic;
 using UnityEngine;
 using static AKCondinoO.Voxels.Water.MarchingCubes.MarchingCubesWater;
 using static AKCondinoO.Voxels.VoxelSystem;
+using System.Runtime.InteropServices;
+
 namespace AKCondinoO.Voxels.Water{
     //  handles data processing in background;
     //  passively gets data from VoxelSystem.Concurrent
     internal class WaterSpreadingContainer:BackgroundContainer{
      internal readonly VoxelWater[]voxelsOutput=new VoxelWater[VoxelsPerChunk];
-     internal Vector2Int cCoord,lastcCoord;
-     internal Vector2Int cnkRgn,lastcnkRgn;
-     internal        int cnkIdx,lastcnkIdx;
+     internal Vector2Int?cCoord,lastcCoord;
+     internal Vector2Int?cnkRgn,lastcnkRgn;
+     internal        int?cnkIdx,lastcnkIdx;
     }
     internal class WaterSpreadingMultithreaded:BaseMultithreaded<WaterSpreadingContainer>{
+        readonly VoxelWater[]voxels=new VoxelWater[VoxelsPerChunk];
         protected override void Cleanup(){
         }
         protected override void Execute(){
-         lock(container.voxelsOutput){
-          //  do not do edges
+         if(container.cnkIdx==null){
+          return;
+         }
+         if(container.lastcnkIdx==null||container.cnkIdx.Value!=container.lastcnkIdx.Value){
+          Array.Clear(voxels,0,voxels.Length);
+         }else{
+          lock(container.voxelsOutput){
+           unsafe{
+            Buffer.BlockCopy(container.voxelsOutput,0,voxels,0,container.voxelsOutput.Length*sizeof(VoxelWater));
+           }
+          }
          }
          //  do edges but lock the voxels output only when reading or writing
+         lock(container.voxelsOutput){
+          unsafe{
+           Buffer.BlockCopy(voxels,0,container.voxelsOutput,0,voxels.Length*sizeof(VoxelWater));
+          }
+         }
         }
     }
 }
