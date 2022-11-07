28076e57dd4b92c1b8ca6d6cda16849fbfa71dfd
Arthur Kenichi Condino Odaguiri <46831317+Arthur-Kenichi-Condino@users.noreply.github.com>
2022-11-07 18:21:51 -0300

    ...
    Former-commit-id: 6227fc10b85aaf182d236153fe541c9d8b8be0bb

--- Assets/AKCondinoO/Voxels/Water/VoxelWaterChunk.cs
+++ Assets/AKCondinoO/Voxels/Water/VoxelWaterChunk.cs
@@ -2,6 +2,7 @@
     #define ENABLE_DEBUG_GIZMOS
     #define ENABLE_LOG_DEBUG
 #endif
+using AKCondinoO.Voxels.Terrain;
 using System;
 using System.Collections;
 using System.Collections.Concurrent;
@@ -11,12 +12,15 @@ using static AKCondinoO.Voxels.Water.MarchingCubes.MarchingCubesWater;
 using static AKCondinoO.Voxels.VoxelSystem;
 namespace AKCondinoO.Voxels.Water{
     internal class VoxelWaterChunk:MonoBehaviour{
+     internal VoxelTerrainChunk tCnk;
      internal WaterSpreadingContainer waterSpreadingBG=new WaterSpreadingContainer();
         internal void OnInstantiated(){
         }
         internal void OnDestroyingCore(){
          waterSpreadingBG.IsCompleted(VoxelSystem.singleton.waterSpreadingBGThreads[0].IsRunning,-1);
         }
+     [SerializeField]float spreadTimeInterval=1.0f;
+     float spreadTimer=1.0f;
      bool waitingMarchingCubes;
      bool pendingMarchingCubes;
      bool waitingWaterSpread;
@@ -34,9 +38,21 @@ namespace AKCondinoO.Voxels.Water{
          }
         }
         bool CanSpreadWater(){
+         spreadTimer-=Time.deltaTime;
+         if(spreadTimer<=0f){
+          //Log.DebugMessage("CanSpreadWater");
+          spreadTimer=spreadTimeInterval;
+          //waterSpreadingBG.cCoord=
+          WaterSpreadingMultithreaded.Schedule(waterSpreadingBG);
+          return true;
+         }
          return false;
         }
         bool OnWaterSpread(){
+         if(waterSpreadingBG.IsCompleted(VoxelSystem.singleton.waterSpreadingBGThreads[0].IsRunning)){
+          //Log.DebugMessage("OnWaterSpread");
+          return true;
+         }
          return false;
         }
     }
