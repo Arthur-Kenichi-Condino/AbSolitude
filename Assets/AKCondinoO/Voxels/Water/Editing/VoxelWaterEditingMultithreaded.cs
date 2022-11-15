12be7d4abd778050db0a0ee0fd82f4805d14c0c1
Arthur Kenichi Condino Odaguiri <46831317+Arthur-Kenichi-Condino@users.noreply.github.com>
2022-11-15 03:02:13 -0300

    ...

--- Assets/AKCondinoO/Voxels/Water/Editing/VoxelWaterEditingMultithreaded.cs
+++ Assets/AKCondinoO/Voxels/Water/Editing/VoxelWaterEditingMultithreaded.cs
@@ -4,8 +4,10 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using static AKCondinoO.Voxels.Water.Editing.VoxelWaterEditing;
 namespace AKCondinoO.Voxels.Water.Editing{
     internal class VoxelWaterEditingContainer:BackgroundContainer{
+     internal readonly Queue<WaterEditRequest>requests=new Queue<WaterEditRequest>();
     }
     internal class VoxelWaterEditingMultithreaded:BaseMultithreaded<VoxelWaterEditingContainer>{
         protected override void Execute(){
