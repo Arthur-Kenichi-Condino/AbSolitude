df18ad1c212abbcb78bc8e399ddda286405e1046
Arthur Kenichi Condino Odaguiri <46831317+Arthur-Kenichi-Condino@users.noreply.github.com>
2022-11-15 03:02:13 -0300

    ...
    Former-commit-id: 12be7d4abd778050db0a0ee0fd82f4805d14c0c1

--- Assets/AKCondinoO/Voxels/Water/Editing/VoxelWaterEditing.cs
+++ Assets/AKCondinoO/Voxels/Water/Editing/VoxelWaterEditing.cs
@@ -19,9 +19,58 @@ namespace AKCondinoO.Voxels.Water.Editing{
          Log.DebugMessage("VoxelWaterEditing:OnDestroyingCoreEvent");
          waterEditingBG.IsCompleted(VoxelSystem.singleton.waterEditingBGThread.IsRunning,-1);
         }
+        internal struct WaterEditRequest{
+         internal Vector3 center;
+        }
+        internal void EditWater(
+         Vector3 at
+        ){
+         editRequests.Enqueue(
+          new WaterEditRequest{
+           center=at
+          }
+         );
+        }
      [SerializeField]bool    DEBUG_ADD_WATER_SOURCE=false;
      [SerializeField]Vector3 DEBUG_ADD_WATER_SOURCE_AT=new Vector3(0,60,0);
+     readonly Queue<WaterEditRequest>editRequests=new Queue<WaterEditRequest>();
+     bool applyingEdits;
         void Update(){
+         if(DEBUG_ADD_WATER_SOURCE){
+            DEBUG_ADD_WATER_SOURCE=false;
+          Log.DebugMessage("DEBUG_ADD_WATER_SOURCE_AT:"+DEBUG_ADD_WATER_SOURCE_AT);
+         }
+         if(applyingEdits){
+             if(OnWaterEditingApplied()){
+                 applyingEdits=false;
+             }
+         }else{
+             if(editRequests.Count>0){
+                 Log.DebugMessage("editRequests.Count>0");
+                 if(OnWaterEditRequestsPush()){
+                     OnWaterEditRequestsPushed();
+                 }
+             }
+         }
+        }
+        bool OnWaterEditRequestsPush(){
+         if(waterEditingBG.IsCompleted(VoxelSystem.singleton.waterEditingBGThread.IsRunning)){
+          while(editRequests.Count>0){
+           waterEditingBG.requests.Enqueue(editRequests.Dequeue());
+          }
+          VoxelWaterEditingMultithreaded.Schedule(waterEditingBG);
+          return true;
+         }
+         return false;
+        }
+        void OnWaterEditRequestsPushed(){
+         applyingEdits=true;
+        }
+        bool OnWaterEditingApplied(){
+         if(waterEditingBG.IsCompleted(VoxelSystem.singleton.waterEditingBGThread.IsRunning)){
+          return true;
+         }
+         return false;
         }
     }
 }
