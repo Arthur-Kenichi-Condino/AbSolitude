7b5dae56f124ce455fd08e1bd16a3e382538745b
Arthur Kenichi Condino Odaguiri <46831317+Arthur-Kenichi-Condino@users.noreply.github.com>
2022-11-16 02:17:42 -0300

    ...
    Former-commit-id: 4b879f5105731407e3c27de9f10597d5601e1b51

--- Assets/AKCondinoO/Voxels/Water/Editing/VoxelWaterEditingMultithreaded.cs
+++ Assets/AKCondinoO/Voxels/Water/Editing/VoxelWaterEditingMultithreaded.cs
@@ -7,6 +7,7 @@ using System.Text;
 using UnityEngine;
 using static AKCondinoO.Voxels.VoxelSystem;
 using static AKCondinoO.Voxels.Water.Editing.VoxelWaterEditing;
+using static AKCondinoO.Voxels.Water.MarchingCubes.MarchingCubesWater;
 namespace AKCondinoO.Voxels.Water.Editing{
     internal class VoxelWaterEditingContainer:BackgroundContainer{
      internal readonly Queue<WaterEditRequest>requests=new Queue<WaterEditRequest>();
@@ -28,11 +29,20 @@ namespace AKCondinoO.Voxels.Water.Editing{
           Vector3    center    =editRequest.center;
           Vector2Int cCoord1=vecPosTocCoord(center );
           Vector2Int cnkRgn1=cCoordTocnkRgn(cCoord1);
+          int cnkIdx1=GetcnkIdx(cCoord1.x,cCoord1.y);
           Vector3Int vCoord1=vecPosTovCoord(center );
+          int vxlIdx1=GetvxlIdx(vCoord1.x,vCoord1.y,vCoord1.z);
           VoxelSystem.Concurrent.water_rwl.EnterReadLock();
           try{
-           //if(VoxelSystem.Concurrent.waterVoxels.TryGetValue()){
-           //}
+           if(VoxelSystem.Concurrent.waterVoxels.TryGetValue(cnkIdx1,out VoxelWater[]voxelsOutput)){
+            lock(voxelsOutput){
+             voxelsOutput[vxlIdx1]=new VoxelWater{
+              density=100d,
+              spreading=100d,
+              absorbing=0d,
+             };
+            }
+           }
           }catch{
            throw;
           }finally{
