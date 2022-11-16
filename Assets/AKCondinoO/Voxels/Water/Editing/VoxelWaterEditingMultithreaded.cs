69153a23faad254b1ae496d85f3d20626f8d43fb
Arthur Kenichi Condino Odaguiri <46831317+Arthur-Kenichi-Condino@users.noreply.github.com>
2022-11-15 22:27:08 -0300

    ...

--- Assets/AKCondinoO/Voxels/Water/Editing/VoxelWaterEditingMultithreaded.cs
+++ Assets/AKCondinoO/Voxels/Water/Editing/VoxelWaterEditingMultithreaded.cs
@@ -3,17 +3,41 @@
 #endif
 using System.Collections;
 using System.Collections.Generic;
+using System.Text;
 using UnityEngine;
+using static AKCondinoO.Voxels.VoxelSystem;
 using static AKCondinoO.Voxels.Water.Editing.VoxelWaterEditing;
 namespace AKCondinoO.Voxels.Water.Editing{
     internal class VoxelWaterEditingContainer:BackgroundContainer{
      internal readonly Queue<WaterEditRequest>requests=new Queue<WaterEditRequest>();
     }
     internal class VoxelWaterEditingMultithreaded:BaseMultithreaded<VoxelWaterEditingContainer>{
+        internal struct WaterEditOutputData{
+         public double density;
+         public double spreading;
+         public double absorbing;
+        }
+     internal readonly Queue<Dictionary<Vector3Int,WaterEditOutputData>>waterEditOutputDataPool=new Queue<Dictionary<Vector3Int,WaterEditOutputData>>();
+     readonly Dictionary<Vector2Int,Dictionary<Vector3Int,WaterEditOutputData>>dataFromFileToMerge=new();
+     readonly Dictionary<Vector2Int,Dictionary<Vector3Int,WaterEditOutputData>>dataForSavingToFile=new();
+     readonly StringBuilder stringBuilder=new StringBuilder();
         protected override void Execute(){
          Log.DebugMessage("VoxelWaterEditingMultithreaded:Execute()");
          while(container.requests.Count>0){
           WaterEditRequest editRequest=container.requests.Dequeue();
+          Vector3    center    =editRequest.center;
+          Vector2Int cCoord1=vecPosTocCoord(center );
+          Vector2Int cnkRgn1=cCoordTocnkRgn(cCoord1);
+          Vector3Int vCoord1=vecPosTovCoord(center );
+          VoxelSystem.Concurrent.water_rwl.EnterReadLock();
+          try{
+           //if(VoxelSystem.Concurrent.waterVoxels.TryGetValue()){
+           //}
+          }catch{
+           throw;
+          }finally{
+           VoxelSystem.Concurrent.water_rwl.ExitReadLock();
+          }
          }
         }
     }
