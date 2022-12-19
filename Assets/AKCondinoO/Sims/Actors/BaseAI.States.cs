42088b635543aaa09d61430194fb42db04207509
Arthur Kenichi Condino Odaguiri <46831317+Arthur-Kenichi-Condino@users.noreply.github.com>
2022-12-19 02:56:50 -0300

    ...
    Former-commit-id: 5c585b1328dcd444a50b62e0736160d2f1ef2189

---
+++ Assets/AKCondinoO/Sims/Actors/BaseAI.States.cs
@@ -0,0 +1,10 @@
+#if UNITY_EDITOR
+    #define ENABLE_LOG_DEBUG
+#endif
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+namespace AKCondinoO.Sims.Actors{
+    internal partial class BaseAI{
+    }
+}
