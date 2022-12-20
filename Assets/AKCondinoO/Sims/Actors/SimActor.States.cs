e5ff1a0204491b53886fc7f56f0c2cae39332879
Arthur Kenichi Condino Odaguiri <46831317+Arthur-Kenichi-Condino@users.noreply.github.com>
2022-12-19 23:38:50 -0300

    ...
    Former-commit-id: c4fb28b0f8f92d49da25e7e097f657848a56b7a7

--- Assets/AKCondinoO/Sims/Actors/SimActor.States.cs
+++ Assets/AKCondinoO/Sims/Actors/SimActor.States.cs
@@ -6,5 +6,37 @@ using System.Collections.Generic;
 using UnityEngine;
 namespace AKCondinoO.Sims.Actors{
     internal partial class SimActor{
+     internal virtual bool isMovingBackwards{
+      get{
+       return false;
+      }
+     }
+     internal virtual float moveVelocityNormalized{
+      get{
+       if(isUsingAI){
+        float velocityMagnitude=navMeshAgent.velocity.magnitude;
+        //Log.DebugMessage("navMeshAgent velocityMagnitude:"+velocityMagnitude);
+        return Mathf.Clamp01(velocityMagnitude/navMeshAgentRunSpeed);
+       }
+       if(simActorCharacterController!=null){
+        float velocityMagnitude=Vector3.Scale(simActorCharacterController.inputMoveVelocity,new Vector3(1f,0f,1f)).magnitude;
+        //Log.DebugMessage("characterController velocityMagnitude:"+velocityMagnitude);
+        return Mathf.Clamp01(velocityMagnitude/(simActorCharacterController.walkSpeedAverage*2f));
+       }
+       return 0f;
+      }
+     }
+     internal float turnAngle{
+      get{
+       if(isUsingAI){
+        if(!Mathf.Approximately(navMeshAgent.velocity.magnitude,0f)){
+         float angle=Vector3.SignedAngle(transform.forward,navMeshAgent.velocity.normalized,transform.up);
+         //Log.DebugMessage("angle:"+angle);
+         return angle;
+        }
+       }
+       return 0f;
+      }
+     }
     }
 }
