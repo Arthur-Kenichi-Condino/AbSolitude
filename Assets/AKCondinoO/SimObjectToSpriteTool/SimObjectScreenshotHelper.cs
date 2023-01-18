e00656a2b55715a89597e57e1cb0f0890ad9b0d4
Arthur Kenichi Condino Odaguiri <46831317+Arthur-Kenichi-Condino@users.noreply.github.com>
2023-01-18 18:19:35 -0300

    Update SimObjectScreenshotHelper.cs
    Former-commit-id: e828256d9098c44e2e61866e4b850b60083cbbc8

--- Assets/AKCondinoO/SimObjectToSpriteTool/SimObjectScreenshotHelper.cs
+++ Assets/AKCondinoO/SimObjectToSpriteTool/SimObjectScreenshotHelper.cs
@@ -5,15 +5,15 @@ namespace AKCondinoO.SimObjectToSpriteTool{
     //  [https://answers.unity.com/questions/733240/how-to-take-a-screenshot-and-apply-it-as-a-texture.html]
     //  [https://answers.unity.com/questions/1473282/show-screen-capture-as-ui-image.html]
     internal class SimObjectScreenshotHelper:MonoBehaviour{
-     private new Camera camera;
      WaitForEndOfFrame waitForEndOfFrame=new WaitForEndOfFrame();
         private IEnumerator TakeScreenshot(){
          yield return waitForEndOfFrame;
-         camera=Camera.main;
+         Camera camera=Camera.main;
          if(camera!=null){
           int sWidth=Screen.width;int sHeight=Screen.height;
           RenderTexture rt=new RenderTexture(sWidth,sHeight,24);
           camera.targetTexture=rt;
+          Texture2D screenshot=new Texture2D(sWidth,sHeight,TextureFormat.RGB24,false);
          }
         }
     }
