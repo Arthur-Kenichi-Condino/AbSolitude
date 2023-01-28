f14323ba99b5ec1cf5f43459d7e45917fccb8ac8
Arthur Kenichi Condino Odaguiri <46831317+Arthur-Kenichi-Condino@users.noreply.github.com>
2023-01-28 11:07:26 -0300

    Update InventoryItem.cs

--- Assets/AKCondinoO/Inventory/InventoryItem.cs
+++ Assets/AKCondinoO/Inventory/InventoryItem.cs
@@ -2,6 +2,6 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 namespace AKCondinoO.Inventory{
-    internal class InventoryItem:MonoBehaviour{
+    internal class InventoryItem{
     }
 }
