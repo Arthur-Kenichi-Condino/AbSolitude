27910178edecd8a08ea9f9527f1c6bf1828e23b7
Arthur Kenichi Condino Odaguiri <46831317+Arthur-Kenichi-Condino@users.noreply.github.com>
2023-02-05 18:54:48 -0300

    ...
    Former-commit-id: 7c62dd216ad7eeee813e8eef5e1c23826176bd51

--- Assets/AKCondinoO/Sims/Actors/Humanoid/Human/HumanAI.cs
+++ Assets/AKCondinoO/Sims/Actors/Humanoid/Human/HumanAI.cs
@@ -1,7 +1,17 @@
+using AKCondinoO.Sims.Inventory;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 namespace AKCondinoO.Sims.Actors.Humanoid.Human{
     internal class HumanAI:HumanoidAI{
+        internal override void OnActivated(){
+         if(Core.singleton.isServer){
+          if(!inventory.ContainsKey(typeof(SimHands2Items))){
+           inventory.Add(typeof(SimHands2Items),new List<SimInventory>());
+           inventory[typeof(SimHands2Items)].Add(new SimHands2Items(this));
+          }
+         }
+         base.OnActivated();
+        }
     }
 }
