e372a775eec76d4d7f16904406babb52e9ad49f4
Arthur Kenichi Condino Odaguiri <46831317+Arthur-Kenichi-Condino@users.noreply.github.com>
2023-02-05 18:56:21 -0300

    ...
    Former-commit-id: 0ec7a684c51164b6a4026c7089c2547c708c861f

--- Assets/AKCondinoO/Sims/Inventory/SimHands.cs
+++ Assets/AKCondinoO/Sims/Inventory/SimHands.cs
@@ -2,8 +2,8 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 namespace AKCondinoO.Sims.Inventory{
-    internal class SimHands2Items:SimInventory{
-        internal SimHands2Items(SimObject owner):base(owner,2){
+    internal class SimHands:SimInventory{
+        internal SimHands(SimObject owner):base(owner,2){
         }
     }
 }
