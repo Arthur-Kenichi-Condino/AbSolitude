efd8d3fc821cdf86a0d2a5fe013a8c47961f6565
Arthur Kenichi Condino Odaguiri <46831317+Arthur-Kenichi-Condino@users.noreply.github.com>
2023-02-10 02:03:36 -0300

    Update SpawnData.cs

--- Assets/AKCondinoO/Sims/SpawnData.cs
+++ Assets/AKCondinoO/Sims/SpawnData.cs
@@ -9,21 +9,24 @@ namespace AKCondinoO.Sims{
      internal readonly List<(Vector3 position,Vector3 rotation,Vector3 scale,Type type,ulong?id,SimObject.PersistentData persistentData)>at;
       internal readonly Dictionary<int,SimActor.PersistentSimActorData>actorData;
        internal readonly Dictionary<int,(Type simType,ulong number)>masters;
-        internal (Type simType,ulong number)?asInventoryItemOwnerId;
+        internal readonly Dictionary<int,(Type simType,ulong number)?>asInventoryItemOwnerIds;
         internal SpawnData(){
          at=new List<(Vector3,Vector3,Vector3,Type,ulong?,SimObject.PersistentData)>(1);
          actorData=new Dictionary<int,SimActor.PersistentSimActorData>(1);
          masters=new Dictionary<int,(Type simType,ulong number)>(1);
+         asInventoryItemOwnerIds=new Dictionary<int,(Type,ulong)?>(1);
         }
         internal SpawnData(int capacity){
          at=new List<(Vector3,Vector3,Vector3,Type,ulong?,SimObject.PersistentData)>(capacity);
          actorData=new Dictionary<int,SimActor.PersistentSimActorData>(capacity);
          masters=new Dictionary<int,(Type simType,ulong number)>(capacity);
+         asInventoryItemOwnerIds=new Dictionary<int,(Type,ulong)?>(capacity);
         }
         internal void Clear(){
          at.Clear();
          actorData.Clear();
          masters.Clear();
+         asInventoryItemOwnerIds.Clear();
         }
     }
 }
