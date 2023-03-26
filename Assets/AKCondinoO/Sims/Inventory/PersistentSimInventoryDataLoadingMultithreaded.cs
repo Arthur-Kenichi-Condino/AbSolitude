8bcab1c75df516b8d7bad3ece81c247571d3c5a3
Arthur Kenichi Condino Odaguiri <46831317+Arthur-Kenichi-Condino@users.noreply.github.com>
2023-03-26 02:55:12 -0300

    ...
    Former-commit-id: 2ec5a07cd14d4f2a18796f4d0d79743cf003f23c

---
+++ Assets/AKCondinoO/Sims/Inventory/PersistentSimInventoryDataLoadingMultithreaded.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+namespace AKCondinoO.Sims.Inventory{
+    internal class PersistentSimInventoryDataLoadingBackgroundContainer:BackgroundContainer{
+    }
+    internal class PersistentSimInventoryDataLoadingMultithreaded:BaseMultithreaded<PersistentSimInventoryDataLoadingBackgroundContainer>{
+        protected override void Cleanup(){
+        }
+        protected override void Execute(){
+        }
+    }
+}
