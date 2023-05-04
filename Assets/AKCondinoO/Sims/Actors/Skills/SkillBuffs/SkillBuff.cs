e3752fadc362d78ee16287ee3fefe4227a650574
Arthur Kenichi Condino Odaguiri <46831317+Arthur-Kenichi-Condino@users.noreply.github.com>
2023-05-04 15:25:05 -0300

    ...

--- Assets/AKCondinoO/Sims/Actors/Skills/SkillBuffs/SkillBuff.cs
+++ Assets/AKCondinoO/Sims/Actors/Skills/SkillBuffs/SkillBuff.cs
@@ -3,5 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 namespace AKCondinoO.Sims.Actors.Skills.SkillBuffs{
     internal class SkillBuff{
+     internal static readonly Queue<SkillBuff>pool=new Queue<SkillBuff>();
+     internal static readonly List<SkillBuff>allActiveBuffs=new List<SkillBuff>();
     }
 }
--- Assets/AKCondinoO/Sims/Actors/Skills/SkillBuffs/SkillBuffEffectsState.cs
+++ Assets/AKCondinoO/Sims/Actors/Skills/SkillBuffs/SkillBuffEffectsState.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 namespace AKCondinoO.Sims.Actors.Skills.SkillBuffs{
     internal class SkillBuffEffectsState:MonoBehaviour{
      internal SimObject targetSimObject;
-     internal List<SkillBuff>activeBuffs=new List<SkillBuff>();
+     internal readonly List<SkillBuff>activeBuffs=new List<SkillBuff>();
         internal void ManualUpdate(float deltaTime){
          foreach(SkillBuff skillBuff in activeBuffs){
          }
