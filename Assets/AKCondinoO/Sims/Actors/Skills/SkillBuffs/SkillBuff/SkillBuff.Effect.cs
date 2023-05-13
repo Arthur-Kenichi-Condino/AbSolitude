f1e25113a8afa5b86dd58ea6892b97baf6bd85ab
Arthur Kenichi Condino Odaguiri <46831317+Arthur-Kenichi-Condino@users.noreply.github.com>
2023-05-13 14:52:03 -0300

    ...

--- Assets/AKCondinoO/Sims/Actors/Skills/SkillBuffs/SkillBuff/SkillBuff.Effect.cs
+++ Assets/AKCondinoO/Sims/Actors/Skills/SkillBuffs/SkillBuff/SkillBuff.Effect.cs
@@ -6,8 +6,11 @@ using System.Collections.Generic;
 using UnityEngine;
 namespace AKCondinoO.Sims.Actors.Skills.SkillBuffs{
     internal partial class SkillBuff{
+     protected Effect effect;
         internal abstract class Effect{
-            internal abstract void Apply();
+            internal abstract void Apply(SimObject.Stats stats);
+            internal abstract bool ApplyRepeating(SimObject.Stats stats);
+            internal abstract void Unapply(SimObject.Stats stats);
         }
     }
 }
