2dda1a0a10f05a7b2ca5a38dd311168c380f06a3
Arthur Kenichi Condino Odaguiri <46831317+Arthur-Kenichi-Condino@users.noreply.github.com>
2023-05-19 18:51:55 -0300

    Update SimObject.Stats.cs

--- Assets/AKCondinoO/Sims/SimObject/SimObject.Stats.cs
+++ Assets/AKCondinoO/Sims/SimObject/SimObject.Stats.cs
@@ -160,106 +160,95 @@ namespace AKCondinoO.Sims{
               protected bool updatedStamina;
             #region MaxStamina
             /// <summary>
-            ///  
+            ///  Energia física máxima para o Sim realizar ações: valor calculado com base em outros atributos
             /// </summary>
-            internal float MaxStaminaGet(SimObject statsSim=null){
-             OnRefresh(statsSim);
-             return maxStamina_value_stats+maxStamina_value_set;
-            }
-             /// <summary>
-             /// 
-             /// </summary>
-             internal void MaxStaminaSet(float value,SimObject statsSim=null,bool forceRefresh=false){
+            protected float maxStamina_value_stats;
+            protected float maxStamina_value_set;
+            protected float maxStamina_value_buffs;
+             internal float MaxStaminaGet(SimObject statsSim=null){
               OnRefresh(statsSim);
-              maxStamina_value_set=value-maxStamina_value_stats;
-              updatedMaxStamina=true;
-              SetPendingRefresh(statsSim,forceRefresh);
+              return maxStamina_value_stats+maxStamina_value_set;
              }
-              float maxStamina_value_stats;
-              float maxStamina_value_set;
-              bool updatedMaxStamina;
+              internal void MaxStaminaSet(float value,SimObject statsSim=null,bool forceRefresh=false){
+               OnRefresh(statsSim);
+               maxStamina_value_set=value-maxStamina_value_stats;
+               updatedMaxStamina=true;
+               SetPendingRefresh(statsSim,forceRefresh);
+              }
+               protected bool updatedMaxStamina;
             #endregion
            #endregion
           #endregion
          #endregion
          #region Sanity
          /// <summary>
-         ///  
+         ///  Sanidade mental ou noção existencial atual para o objeto Sim: se este valor chegar a 0f, o Sim irá desmaiar ou perder o controle das ações
          /// </summary>
-         internal float SanityGet(SimObject statsSim=null){
-          OnRefresh(statsSim);
-          return sanity_value;
-         }
-          /// <summary>
-          ///  Função set
-          /// </summary>
-          internal void SanitySet(float value,SimObject statsSim=null,bool forceRefresh=false){
-           sanity_value=value;
-           updatedSanity=true;
-           SetPendingRefresh(statsSim,forceRefresh);
+         protected float sanity_value;
+          internal float SanityGet(SimObject statsSim=null){
+           OnRefresh(statsSim);
+           return sanity_value;
           }
-           float sanity_value;
-           bool updatedSanity;
+           internal void SanitySet(float value,SimObject statsSim=null,bool forceRefresh=false){
+            sanity_value=value;
+            updatedSanity=true;
+            SetPendingRefresh(statsSim,forceRefresh);
+           }
+            protected bool updatedSanity;
           #region MaxSanity
           /// <summary>
           ///  
           /// </summary>
-          internal float MaxSanityGet(SimObject statsSim=null){
-           OnRefresh(statsSim);
-           return maxSanity_value_stats+maxSanity_value_set;
-          }
-           /// <summary>
-           /// 
-           /// </summary>
-           internal void MaxSanitySet(float value,SimObject statsSim=null,bool forceRefresh=false){
+          protected float maxSanity_value_stats;
+          protected float maxSanity_value_set;
+          protected float maxSanity_value_buffs;
+           internal float MaxSanityGet(SimObject statsSim=null){
             OnRefresh(statsSim);
-            maxSanity_value_set=value-maxSanity_value_stats;
-            updatedMaxSanity=true;
-            SetPendingRefresh(statsSim,forceRefresh);
+            return maxSanity_value_stats+maxSanity_value_set;
            }
-            float maxSanity_value_stats;
-            float maxSanity_value_set;
-            bool updatedMaxSanity;
-             #region Focus
-             /// <summary>
-             ///  
-             /// </summary>
-             internal float FocusGet(SimObject statsSim=null){
-              OnRefresh(statsSim);
-              return focus_value;
+            internal void MaxSanitySet(float value,SimObject statsSim=null,bool forceRefresh=false){
+             OnRefresh(statsSim);
+             maxSanity_value_set=value-maxSanity_value_stats;
+             updatedMaxSanity=true;
+             SetPendingRefresh(statsSim,forceRefresh);
+            }
+             protected bool updatedMaxSanity;
+           #region Focus
+           /// <summary>
+           ///  
+           /// </summary>
+           protected float focus_value;
+            internal float FocusGet(SimObject statsSim=null){
+             OnRefresh(statsSim);
+             return focus_value;
+            }
+             internal void FocusSet(float value,SimObject statsSim=null,bool forceRefresh=false){
+              focus_value=value;
+              updatedFocus=true;
+              SetPendingRefresh(statsSim,forceRefresh);
              }
-              /// <summary>
-              /// 
-              /// </summary>
-              internal void FocusSet(float value,SimObject statsSim=null,bool forceRefresh=false){
-               focus_value=value;
-               pendingRefresh=true;
-               if(forceRefresh){
-                OnRefresh(statsSim);
-               }
-              }
-               float focus_value;
-              #region MaxFocus
-              /// <summary>
-              ///  
-              /// </summary>
-              internal float MaxFocusGet(SimObject statsSim=null){
+              protected bool updatedFocus;
+            #region MaxFocus
+            /// <summary>
+            ///  
+            /// </summary>
+            internal float MaxFocusGet(SimObject statsSim=null){
+             OnRefresh(statsSim);
+             return maxFocus_value;
+            }
+             /// <summary>
+             /// 
+             /// </summary>
+             internal void MaxFocusSet(float value,SimObject statsSim=null,bool forceRefresh=false){
+              maxFocus_value=value;
+              pendingRefresh=true;
+              if(forceRefresh){
                OnRefresh(statsSim);
-               return maxFocus_value;
               }
-               /// <summary>
-               /// 
-               /// </summary>
-               internal void MaxFocusSet(float value,SimObject statsSim=null,bool forceRefresh=false){
-                maxFocus_value=value;
-                pendingRefresh=true;
-                if(forceRefresh){
-                 OnRefresh(statsSim);
-                }
-               }
-                float maxFocus_value;
-              #endregion
-             #endregion
+             }
+              float maxFocus_value;
+            #endregion
+           #endregion
           #endregion
          #endregion
          //
