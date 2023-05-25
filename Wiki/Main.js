c29af7e52630a86b092e84e654f6e6653f82c8ca
motinha122 <roblox-tiago@hotmail.com>
2023-05-24 23:41:17 -0300

    Text splited in lines

--- Wiki/Main.js
+++ Wiki/Main.js
@@ -1,7 +1,23 @@
-async function ParserText(path) {
+//Paths
+
+const root = "WikiContent/";
+const homunculusSystem = "HomunculusSystem/";
+const selectedHomunculus = "Vanilmirth/";
+
+async function FetchText(path) {
     const response = await fetch(path);
-    const data = await response.text();
-    console.log(data);
+    const rawText = await response.text();
+    const stringText = rawText.toString();
+    const splitedText = stringText.split("\n");
+    return splitedText;
 }
 
-ParserText("WikiContent/HomunculusSystem/Vanilmirth/Content.txt");
+const text = await FetchText(root + homunculusSystem + selectedHomunculus + "Content.txt");
+
+function ParseText(text) {
+    for (line of text) {
+        console.log(line);
+    }
+}
+
+ParseText(text);
