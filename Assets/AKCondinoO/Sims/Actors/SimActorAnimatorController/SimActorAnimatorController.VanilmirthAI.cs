8a023e5ba9bb48d6b73b3777f71111f192ba2fb3
Arthur Kenichi Condino Odaguiri <46831317+Arthur-Kenichi-Condino@users.noreply.github.com>
2023-06-03 06:24:30 -0300

    adicionado efeito sonoro para habilidade Cura Espiritual
    Former-commit-id: 2eb77247f74768c97462c804d0c1fa66d204a1ff

--- Assets/AKCondinoO/Sims/Actors/SimActorAnimatorController/SimActorAnimatorController.VanilmirthAI.cs
+++ Assets/AKCondinoO/Sims/Actors/SimActorAnimatorController/SimActorAnimatorController.VanilmirthAI.cs
@@ -11,6 +11,10 @@ namespace AKCondinoO.Sims.Actors{
         internal void UpdateVanilmirthAIAnimatorWeaponLayer(BaseAI baseAI,VanilmirthAI vanilmirthAI){
         }
         internal void UpdateVanilmirthAIAnimatorMotionValue(BaseAI baseAI,VanilmirthAI vanilmirthAI){
+               animator.SetBool("MOTION_STAND",vanilmirthAI.motion==BaseAI.ActorMotion.MOTION_STAND);
+               animator.SetBool("MOTION_MOVE" ,vanilmirthAI.motion==BaseAI.ActorMotion.MOTION_MOVE );
+                animator.SetFloat("MOTION_MOVE_VELOCITY",vanilmirthAI.moveVelocityFlattened);
+                 animator.SetFloat("MOTION_MOVE_TURN",vanilmirthAI.turnAngle/180f);
         }
     }
 }
