c4c2bd1162ca3c3f5d0cf5d6ce355fafc4e33dc8
Arthur Kenichi Condino Odaguiri <46831317+Arthur-Kenichi-Condino@users.noreply.github.com>
2023-06-09 23:05:43 -0300

    ...
    Former-commit-id: af0ebe66c7d55cf063839dc45eef3308d0a697bf

---
+++ Assets/AKCondinoO/Sims/SimsMachine/SimsMachineSpawnSettings.cs
@@ -0,0 +1,10 @@
+#if UNITY_EDITOR
+    #define ENABLE_LOG_DEBUG
+#endif
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+namespace AKCondinoO.Sims{
+    internal class SimsMachineSpawnSettings{
+    }
+}
