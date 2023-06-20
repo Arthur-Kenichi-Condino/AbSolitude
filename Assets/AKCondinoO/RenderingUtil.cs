bc5c323f2519ba03db834cb88b3d5a0b25b27491
Arthur Kenichi Condino Odaguiri <46831317+Arthur-Kenichi-Condino@users.noreply.github.com>
2023-06-20 02:07:20 -0300

    ...

--- Assets/AKCondinoO/RenderingUtil.cs
+++ Assets/AKCondinoO/RenderingUtil.cs
@@ -25,9 +25,9 @@ namespace AKCondinoO{
          switch(blendMode){
           case BlendMode.Opaque:{
            standardShaderMaterial.SetFloat("_Mode",0);
-           standardShaderMaterial.SetInt("_SrcBlend",(int)UnityEngine.Rendering.BlendMode.One);
-           standardShaderMaterial.SetInt("_DstBlend",(int)UnityEngine.Rendering.BlendMode.Zero);
-           standardShaderMaterial.SetInt("_ZWrite",1);
+           standardShaderMaterial.SetFloat("_SrcBlend",(int)UnityEngine.Rendering.BlendMode.One);
+           standardShaderMaterial.SetFloat("_DstBlend",(int)UnityEngine.Rendering.BlendMode.Zero);
+           standardShaderMaterial.SetFloat("_ZWrite",1);
            standardShaderMaterial.DisableKeyword("_ALPHATEST_ON");
            standardShaderMaterial.DisableKeyword("_ALPHABLEND_ON");
            standardShaderMaterial.DisableKeyword("_ALPHAPREMULTIPLY_ON");
@@ -36,9 +36,9 @@ namespace AKCondinoO{
           }
           case BlendMode.Cutout:{
            standardShaderMaterial.SetFloat("_Mode",1);
-           standardShaderMaterial.SetInt("_SrcBlend",(int)UnityEngine.Rendering.BlendMode.One);
-           standardShaderMaterial.SetInt("_DstBlend",(int)UnityEngine.Rendering.BlendMode.Zero);
-           standardShaderMaterial.SetInt("_ZWrite",1);
+           standardShaderMaterial.SetFloat("_SrcBlend",(int)UnityEngine.Rendering.BlendMode.One);
+           standardShaderMaterial.SetFloat("_DstBlend",(int)UnityEngine.Rendering.BlendMode.Zero);
+           standardShaderMaterial.SetFloat("_ZWrite",1);
            standardShaderMaterial.EnableKeyword("_ALPHATEST_ON");
            standardShaderMaterial.DisableKeyword("_ALPHABLEND_ON");
            standardShaderMaterial.DisableKeyword("_ALPHAPREMULTIPLY_ON");
@@ -47,9 +47,9 @@ namespace AKCondinoO{
           }
           case BlendMode.Fade:{
            standardShaderMaterial.SetFloat("_Mode",2);
-           standardShaderMaterial.SetInt("_SrcBlend",(int)UnityEngine.Rendering.BlendMode.SrcAlpha);
-           standardShaderMaterial.SetInt("_DstBlend",(int)UnityEngine.Rendering.BlendMode.OneMinusSrcAlpha);
-           standardShaderMaterial.SetInt("_ZWrite",0);
+           standardShaderMaterial.SetFloat("_SrcBlend",(int)UnityEngine.Rendering.BlendMode.SrcAlpha);
+           standardShaderMaterial.SetFloat("_DstBlend",(int)UnityEngine.Rendering.BlendMode.OneMinusSrcAlpha);
+           standardShaderMaterial.SetFloat("_ZWrite",0);
            standardShaderMaterial.DisableKeyword("_ALPHATEST_ON");
            standardShaderMaterial.EnableKeyword("_ALPHABLEND_ON");
            standardShaderMaterial.DisableKeyword("_ALPHAPREMULTIPLY_ON");
@@ -58,9 +58,9 @@ namespace AKCondinoO{
           }
           case BlendMode.Transparent:{
            standardShaderMaterial.SetFloat("_Mode",3);
-           standardShaderMaterial.SetInt("_SrcBlend",(int)UnityEngine.Rendering.BlendMode.One);
-           standardShaderMaterial.SetInt("_DstBlend",(int)UnityEngine.Rendering.BlendMode.OneMinusSrcAlpha);
-           standardShaderMaterial.SetInt("_ZWrite",0);
+           standardShaderMaterial.SetFloat("_SrcBlend",(int)UnityEngine.Rendering.BlendMode.One);
+           standardShaderMaterial.SetFloat("_DstBlend",(int)UnityEngine.Rendering.BlendMode.OneMinusSrcAlpha);
+           standardShaderMaterial.SetFloat("_ZWrite",0);
            standardShaderMaterial.DisableKeyword("_ALPHATEST_ON");
            standardShaderMaterial.DisableKeyword("_ALPHABLEND_ON");
            standardShaderMaterial.EnableKeyword("_ALPHAPREMULTIPLY_ON");
