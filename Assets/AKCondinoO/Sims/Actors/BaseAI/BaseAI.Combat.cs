31fa0e98561c4af1e640e8846afc5a6b74a8a1d8
Arthur Kenichi Condino Odaguiri <46831317+Arthur-Kenichi-Condino@users.noreply.github.com>
2023-06-27 00:49:37 -0300

    inimigos agora perseguem corretamente seus alvos
    Former-commit-id: e4289c1c33c7a1307c4f4f3e2d80be2936b7947f

--- Assets/AKCondinoO/Sims/Actors/BaseAI/BaseAI.Combat.cs
+++ Assets/AKCondinoO/Sims/Actors/BaseAI/BaseAI.Combat.cs
@@ -7,7 +7,7 @@ using System.Collections.Generic;
 using UnityEngine;
 namespace AKCondinoO.Sims.Actors{
     internal partial class BaseAI{
-     protected Vector3 MyAttackRange=new Vector3(0f,0f,.25f);internal Vector3 attackRange{get{return MyAttackRange;}}
+     protected Vector3 MyAttackRange=new Vector3(0f,.4375f,.25f);internal Vector3 attackRange{get{return MyAttackRange;}}
      protected SimObject MyEnemy=null;internal SimObject enemy{get{return MyEnemy;}}
         internal enum AggressionMode:int{
          Defensive=0,
@@ -58,7 +58,7 @@ namespace AKCondinoO.Sims.Actors{
          float disXZPlane=new Vector3(delta.x,0f,delta.z).magnitude;
          float radius=Mathf.Max(localBounds.extents.x,localBounds.extents.z);
          float simObjectRadius=Mathf.Max(simObject.localBounds.extents.x,simObject.localBounds.extents.z);
-         if(disXZPlane<=radius+simObjectRadius+MyAttackRange.z){
+         if(disXZPlane<=radius+simObjectRadius+MyAttackRange.z&&delta.y<=localBounds.extents.y+MyAttackRange.y){
           //Log.DebugMessage("simObject is in my attack range:disXZPlane:"+disXZPlane);
           return true;
          }
