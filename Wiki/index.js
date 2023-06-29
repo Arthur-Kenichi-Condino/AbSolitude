91c7a349b7d1111bdeee0260e1f24abf8881e392
motinha122 <roblox-tiago@hotmail.com>
2023-06-29 13:39:43 -0300

    function to create DOM
    Former-commit-id: 54a5a4c48bfc61d4205c33d8c291b5c77c325d6b

--- Wiki/index.js
+++ Wiki/index.js
@@ -5,50 +5,64 @@ const homunculusSystem = "HomunculusSystem";
 const selectedHomunculus = "Vanilmirth";
 
 async function FetchText(path) {
-    const response = await fetch(path);
-    const rawText = await response.text();
-    const stringText = rawText.toString();
-    const splitedText = stringText.split("\n");
-    return splitedText;
+  const response = await fetch(path);
+  const rawText = await response.text();
+  const stringText = rawText.toString();
+  const splitedText = stringText.split("\n");
+  return splitedText;
 }
 
-const text = await FetchText(root + "/" + homunculusSystem + "/" + selectedHomunculus + "/" + "Content.txt");
+const text = await FetchText(
+  root + "/" + homunculusSystem + "/" + selectedHomunculus + "/" + "Content.txt"
+);
 
 //Parse text functions
 
-function RemoveCharAt(string,index) {
-    return string.slice(0, index) + string.slice(index + 1);
+function RemoveCharAt(string, index) {
+  return string.slice(0, index) + string.slice(index + 1);
 }
 
-function SetCharAt(string,index,char){
-    return string.substr(0, index) + char + string.substr(index + 1);
+function SetCharAt(string, index, char) {
+  return string.substr(0, index) + char + string.substr(index + 1);
 }
 
-// DOM element using Jquery 
+// DOM element using Jquery
 
-function CreateDOMelement(parent,child,content,index,className) {
-    const element = document.createElement(child);
-    $(parent).append(element);
-    $(element).text(content);
-    $(element).addClass(`${className} ${className}_${index}`);
-    console.log(`Element created: ${child} - ${index}`);
+function CreateDOMelement(options, content, index) {
+  
+  const element = document.createElement(options.child);
+  $(options.parent).append(element);
+  $(element).text(content);
+  $(element).addClass(`${options.className} ${options.className}_${index}`);
+  console.log(`Element created: ${options.child} - ${index}`);
 }
 
-//Parse text 
+// DOM create element object 
+
+function CreateDOMobject(parent, child, className) {
+  this.parent = parent;
+  this.child = child;
+  this.className = className;
+}
+
+//Parse text
+
+const titleObj = new CreateDOMobject(".wiki", "h2", "title");
+const textObj = new CreateDOMobject(".wiki", "section", "text");
 
 function ParseText(text) {
-    let titleIndex = 0 ,textIndex = 0;
-    for (const line of text) {
-        if (line.charAt(0) == "#") {
-            let title = RemoveCharAt(line, 0);
-            CreateDOMelement(".wiki","h2", title, titleIndex, "title");
-            titleIndex++;
-        } else {
-            CreateDOMelement(".wiki","section",line,textIndex,"text")
-            textIndex++;
-        }
+  let titleIndex = 0,
+    textIndex = 0;
+  for (const line of text) {
+    if (line.charAt(0) == "#") {
+      let title = RemoveCharAt(line, 0);
+      CreateDOMelement(titleObj, title, titleIndex);
+      titleIndex++;
+    } else {
+      CreateDOMelement(textObj, line, textIndex);
+      textIndex++;
     }
+  }
 }
 
 ParseText(text);
-
