461248ccdfb769514aab6225d7f7d433c7bdb785
Arthur Kenichi Condino Odaguiri <46831317+Arthur-Kenichi-Condino@users.noreply.github.com>
2023-08-01 18:11:50 -0300

    animações
    Former-commit-id: 8a362fe73b77635428a9af3786044d48c5d54d02

--- Assets/AKCondinoO/Sims/SimObjectAsInventoryItemSettings.cs
+++ Assets/AKCondinoO/Sims/SimObjectAsInventoryItemSettings.cs
@@ -5,18 +5,25 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using static AKCondinoO.Sims.Actors.BaseAI;
 using static AKCondinoO.Sims.Actors.SimActor;
 using static AKCondinoO.Sims.Inventory.SimInventoryItemsInContainerSettings;
 namespace AKCondinoO.Sims.Inventory{
     internal class SimObjectAsInventoryItemSettings:MonoBehaviour{
      [SerializeField]internal HandsUsage handsUsage;
      [SerializeField]internal WeaponTypes weaponType;
-     [SerializeField]internal Transform  leftHandGrabTransform;
-     [SerializeField]internal Transform rightHandGrabTransform;
-        [Serializable]public struct SimInventoryItemInContainerData{
+     //  TO DO: create a "fallback" transform for any motion, which is the transform for a container that doesn't need to change by sim actor's motion
+     [SerializeField]internal SimInventoryItemInContainerData[]inventorySettings;
+        [Serializable]internal class SimInventoryItemInContainerData{
          [SerializeField]internal string simInventoryType;
          [SerializeField]internal int simInventorySpaceUse;
+         [SerializeField]internal SimObjectAsInventoryItemTransformForMotion[]transformSettingsForMotion;
+            [Serializable]internal class SimObjectAsInventoryItemTransformForMotion{
+             [SerializeField]internal ActorMotion motion;
+             [SerializeField]internal string parentBodyPartName;
+             [SerializeField]internal bool transformIsDeterminant;
+             [SerializeField]internal Transform transform;
+            }
         }
-     [SerializeField]internal SimInventoryItemInContainerData[]inventorySettings;
     }
 }
