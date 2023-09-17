c67b60e7ffa861e1a6c07ab11824f493c761364c
Arthur "Gibraltar" Kenichi Condino Odaguiri <46831317+Arthur-Kenichi-Condino@users.noreply.github.com>
2023-09-17 14:39:52 -0300

    ...

--- Assets/AKCondinoO/Sims/Actors/_SimActor/_BaseAI/AIUtil.cs
+++ Assets/AKCondinoO/Sims/Actors/_SimActor/_BaseAI/AIUtil.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 namespace AKCondinoO.Sims.Actors{
-    internal static class BaseAIUtil{
+    internal static class AIUtil{
         internal enum GetValue:int{
          V_MOTION     = 3,
          V_STATE      =15,
