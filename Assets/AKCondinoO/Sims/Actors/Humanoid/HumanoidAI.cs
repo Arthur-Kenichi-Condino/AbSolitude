3a1a90b801caa4379cecc9a677bbd9be6b7467b0
Arthur "Gibraltar" Kenichi Condino Odaguiri <46831317+Arthur-Kenichi-Condino@users.noreply.github.com>
2023-10-05 14:21:15 -0300

    ...

--- Assets/AKCondinoO/Sims/Actors/Humanoid/HumanoidAI.cs
+++ Assets/AKCondinoO/Sims/Actors/Humanoid/HumanoidAI.cs
@@ -7,6 +7,7 @@ namespace AKCondinoO.Sims.Actors.Humanoid{
         protected override void OnUMACharacterUpdated(UMAData simUMAData){
          Log.DebugMessage("OnUMACharacterUpdated");
          SetBodyPart("rightHand","hand.R",out _);
+         SetBodyPart("abdomen","spine.001",out _);
          base.OnUMACharacterUpdated(simUMAData);
         }
     }
