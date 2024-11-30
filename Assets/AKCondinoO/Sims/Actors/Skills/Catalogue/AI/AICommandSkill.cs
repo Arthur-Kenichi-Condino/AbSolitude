bc9cd24f675cd0e7405b5c1f5e2d04f705d16535
Arthur "Gibraltar" Kenichi Condino Odaguiri <46831317+Arthur-Kenichi-Condino@users.noreply.github.com>
2024-11-30 09:30:28 -0300

    ...

---
+++ Assets/AKCondinoO/Sims/Actors/Skills/Catalogue/AI/AICommandSkill.cs
@@ -0,0 +1,12 @@
+#if UNITY_EDITOR
+    #define ENABLE_LOG_DEBUG
+#endif
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+namespace AKCondinoO.Sims.Actors.Skills{
+    internal class AICommandSkill:Skill{
+    }
+}
