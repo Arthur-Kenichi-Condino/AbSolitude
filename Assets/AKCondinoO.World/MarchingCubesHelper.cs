1837d082c4173789e1e5c903fc715699abf8f47c
Arthur-Kenichi-Condino <46831317+Arthur-Kenichi-Condino@users.noreply.github.com>
2026-04-29 22:41:40 -0300

    organização

---
+++ Assets/AKCondinoO.World/MarchingCubesHelper.cs
@@ -0,0 +1,38 @@
+using UnityEngine;
+namespace AKCondinoO.World.MarchingCubes{
+    internal static class MarchingCubesHelper{
+        //internal static float SampleDensity(
+        //){
+        //}
+        //internal static double SampleDensity(
+        //){
+        //}
+        //internal static bool TryFindSurfaceTopDown(
+        // Vector3Int xz,
+        // int startY,
+        // int minY,
+        // out Vector3 hitPoint,
+        // out Vector3 normal
+        //){
+        // float prevDensity=SampleDensity(new Vector3Int(xz.x,startY,xz.y));
+        // for(int y=startY-1;y>=minY;y--){
+        //  var coord=new Vector3Int(xz.x,y,xz.y);
+        //  float d=SampleDensity(coord);
+        //  //  Detecta crossing (iso surface)
+        //  if(prevDensity>isoLevel&&d<=isoLevel){
+        //   normal=SampleNormal(coord);
+        //   if(Vector3.Dot(normal,Vector3.up)<=0f){
+        //    prevDensity=d;
+        //    continue;
+        //   }
+        //   hitPoint=coord;
+        //   return true;
+        //  }
+        //  prevDensity=d;
+        // }
+        // hitPoint=default;
+        // normal=default;
+        // return false;
+        //}
+    }
+}
