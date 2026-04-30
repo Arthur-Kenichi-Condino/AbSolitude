9785a7f106e74f4f7db04c71add6ef5aae392bea
Arthur-Kenichi-Condino <46831317+Arthur-Kenichi-Condino@users.noreply.github.com>
2026-04-30 14:41:35 -0300

    ...

--- Assets/AKCondinoO.World/BiomesConfigurationSnapshot.cs
+++ Assets/AKCondinoO.World/BiomesConfigurationSnapshot.cs
@@ -106,44 +106,125 @@ namespace AKCondinoO.World{
          Snapshot.pool.Return(snapshot);snapshot=null;
         }
         internal static void SetTerrainvxl(ref Voxel vxl,Vector3Int vCoord,Vector2Int cCoord,BiomesConfigurationContext context){
-         var snapshot=BiomesConfigurationSnapshot.snapshot;
          /*  fora do mundo, baixo:  */
          if(vCoord.y<=0){
           vxl=Voxel.bedrock;
           return;
+         }
          /*  fora do mundo, cima:  */
-         }else if(vCoord.y>=Height){
+         if(vCoord.y>=Height){
           vxl=Voxel.air;
           return;
          }
-         ValidatevCoord(ref cCoord,ref vCoord);
-         Vector2Int cnkRgn=cCoordTocnkRgn(cCoord);
-         Vector3Int noiseInputRounded=vCoord+new Vector3Int(cnkRgn.x,0,cnkRgn.y);
-         Vector3    noiseInput       =noiseInputRounded+new Vector3(.5f,.5f,.5f);
-         double heightValue=-1d;
-         if(!context.TryUseTerrainHeightNoiseCache(out heightValue)){
-          heightValue=snapshot.graph.GetValue(NoiseChannel.TerrainHeight,new(noiseInput.z,noiseInput.x,0));
-          context.UpdateTerrainHeightNoiseCache(heightValue);
+         var sampleInput=new SampleContext(vCoord,cCoord);
+         var sampleDensityContext=new SampleDensityContext(sampleInput);
+         sampleDensityContext.hasHeight=context.TryUseTerrainHeightNoiseCache(out sampleDensityContext.heightValue);
+         float density=SampleDensity(ref sampleDensityContext,out bool updateTerrainHeightNoiseCache);
+         if(updateTerrainHeightNoiseCache){
+          context.UpdateTerrainHeightNoiseCache(sampleDensityContext.heightValue);
          }
+         if(density<=Voxel.air.density){
+          vxl=Voxel.air;
+          return;
+         }
+         var sampleMaterialContext=new SampleMaterialContext(sampleInput);
+         var material=SampleMaterial(ref sampleMaterialContext);
+         vxl=new(
+          density,
+          material,
+          Vector3.zero
+         );
+        }
+    internal struct SampleContext{
+     public Vector3Int vCoord;
+     public Vector2Int cCoord;
+     public Vector2Int cnkRgn;
+     public Vector3Int noiseInputRounded;
+     public Vector3    noiseInput       ;
+        internal SampleContext(Vector3Int vCoord,Vector2Int cCoord){
+         ValidatevCoord(ref cCoord,ref vCoord);
+         this.vCoord=vCoord;
+         this.cCoord=cCoord;
+         cnkRgn=cCoordTocnkRgn(cCoord);
+         noiseInputRounded=vCoord+new Vector3Int(cnkRgn.x,0,cnkRgn.y);
+         noiseInput       =noiseInputRounded+new Vector3(.5f,.5f,.5f);
+        }
+    }
+    internal struct SampleDensityContext{
+     public Vector3Int vCoord;
+     public Vector2Int cCoord;
+     public Vector2Int cnkRgn;
+     public Vector3Int noiseInputRounded;
+     public Vector3    noiseInput       ;
+     public bool hasHeight;
+     public double heightValue;
+        internal SampleDensityContext(SampleContext input){
+         vCoord=input.vCoord;
+         cCoord=input.cCoord;
+         cnkRgn=input.cnkRgn;
+         noiseInputRounded=input.noiseInputRounded;
+         noiseInput       =input.noiseInput       ;
+         hasHeight=false;
+         heightValue=-1f;
+        }
+    }
+        internal static float SampleDensity(ref SampleDensityContext context,out bool computed){
+         var snapshot=BiomesConfigurationSnapshot.snapshot;
+         computed=false;
+         Vector3Int vCoord=context.vCoord;
+         Vector3Int noiseInputRounded=context.noiseInputRounded;
+         Vector3    noiseInput       =context.noiseInput       ;
+         if(vCoord.y<=0)
+          return Voxel.bedrock.density;
+         if(vCoord.y>=Height)
+          return Voxel.air.density;
+         if(!context.hasHeight){
+          context.heightValue=snapshot.graph.GetValue(
+           NoiseChannel.TerrainHeight,
+           new(noiseInput.z,noiseInput.x,0)
+          );
+          computed=true;
+          context.hasHeight=true;
+         }
+         double heightValue=context.heightValue;
          if(heightValue>=0d){
-          var materialTable=snapshot.graph.GetMaterialTable(NoiseChannel.TerrainSurfaceMaterial,new(noiseInput.z,noiseInput.x,0));
-          MaterialId material=MaterialId.MuddyDirt;
-          if(materialTable!=null){material=materialTable.baseMaterial;}
           if(noiseInputRounded.y<=heightValue+snapshot.terrainSmoothingHeight){
            float density=100.0f;
            float delta=(float)heightValue-noiseInputRounded.y;
            float smoothingValue=(snapshot.terrainSmoothingHeight-delta)/snapshot.terrainSmoothingHeight;
            density*=1f-smoothingValue;
            density=Mathf.Clamp(density,0f,100.0f);
-           vxl=new(
-            density,
-            material,
-            Vector3.zero
-           );
-           return;
+           return density;
           }
          }
-         vxl=Voxel.air;
+         return Voxel.air.density;
+        }
+    internal struct SampleMaterialContext{
+     public Vector3Int vCoord;
+     public Vector2Int cCoord;
+     public Vector2Int cnkRgn;
+     public Vector3Int noiseInputRounded;
+     public Vector3    noiseInput       ;
+        internal SampleMaterialContext(SampleContext input){
+         vCoord=input.vCoord;
+         cCoord=input.cCoord;
+         cnkRgn=input.cnkRgn;
+         noiseInputRounded=input.noiseInputRounded;
+         noiseInput       =input.noiseInput       ;
+        }
+    }
+        internal static MaterialId SampleMaterial(
+         ref SampleMaterialContext context
+        ){
+         var snapshot=BiomesConfigurationSnapshot.snapshot;
+         Vector3Int vCoord=context.vCoord;
+         Vector3Int noiseInputRounded=context.noiseInputRounded;
+         Vector3    noiseInput       =context.noiseInput       ;
+         var table=snapshot.graph.GetMaterialTable(
+          NoiseChannel.TerrainSurfaceMaterial,
+          new(noiseInput.z,noiseInput.x,0)
+         );
+         return table!=null?table.baseMaterial:MaterialId.MuddyDirt;
         }
         internal static SnapshotSpawnSettings GetSpawnSettings(NoiseChannel channel){
          var snapshotSpawnSettings=snapshot.graph.GetSpawnSettings(channel);
--- Assets/AKCondinoO.World/MarchingCubesHelper.cs
+++ Assets/AKCondinoO.World/MarchingCubesHelper.cs
@@ -1,38 +1,37 @@
 using UnityEngine;
+using static AKCondinoO.World.BiomesConfigurationSnapshot;
+using static AKCondinoO.World.WorldChunkManagerConst;
 namespace AKCondinoO.World.MarchingCubes{
     internal static class MarchingCubesHelper{
-        //internal static float SampleDensity(
-        //){
-        //}
-        //internal static double SampleDensity(
-        //){
-        //}
-        //internal static bool TryFindSurfaceTopDown(
-        // Vector3Int xz,
-        // int startY,
-        // int minY,
-        // out Vector3 hitPoint,
-        // out Vector3 normal
-        //){
-        // float prevDensity=SampleDensity(new Vector3Int(xz.x,startY,xz.y));
-        // for(int y=startY-1;y>=minY;y--){
-        //  var coord=new Vector3Int(xz.x,y,xz.y);
-        //  float d=SampleDensity(coord);
-        //  //  Detecta crossing (iso surface)
-        //  if(prevDensity>isoLevel&&d<=isoLevel){
-        //   normal=SampleNormal(coord);
-        //   if(Vector3.Dot(normal,Vector3.up)<=0f){
-        //    prevDensity=d;
-        //    continue;
-        //   }
-        //   hitPoint=coord;
-        //   return true;
-        //  }
-        //  prevDensity=d;
-        // }
-        // hitPoint=default;
-        // normal=default;
-        // return false;
-        //}
+        internal static bool TryFindSurfaceTopDown(
+         Vector3Int vCoord,Vector2Int cCoord,
+         int startY,
+         int bottomY,
+         out Vector3 hitPoint,
+         out Vector3 normal,
+         float isoLevel=-50.0f
+        ){
+         var sampleInput=new SampleContext(vCoord,cCoord);
+         var sampleDensityContext=new SampleDensityContext(sampleInput);
+         double prevDensity=SampleDensity(ref sampleDensityContext,out _);
+         //for(int y=startY-1;y>=bottomY;y--){
+         // var coord=new Vector3Int(vCoord.x,y,vCoord.z);
+         // double d=SampleDensity(coord,cCoord,);
+         // //  Detecta crossing (iso surface)
+         // if(prevDensity>isoLevel&&d<=isoLevel){
+         //  normal=SampleNormal(coord);
+         //  if(Vector3.Dot(normal,Vector3.up)<=0f){
+         //   prevDensity=d;
+         //   continue;
+         //  }
+         //  hitPoint=coord;
+         //  return true;
+         // }
+         // prevDensity=d;
+         //}
+         hitPoint=default;
+         normal=default;
+         return false;
+        }
     }
 }
